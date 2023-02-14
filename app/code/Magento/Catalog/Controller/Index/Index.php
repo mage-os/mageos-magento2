dde368c160e8db739f89cf1c9a65ea1b9aa3bb5c
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-02-14 19:47:23 +0530

    phpcs testing

--- app/code/Magento/Catalog/Controller/Index/Index.php
+++ app/code/Magento/Catalog/Controller/Index/Index.php
@@ -20,6 +20,7 @@ class Index extends \Magento\Framework\App\Action\Action implements HttpGetActio
     public function execute()
     {
         echo "123";
+
         echo "11 121 d dsd d2 test 11"
         $resultRedirect = $this->resultRedirectFactory->create();
         return $resultRedirect->setPath('/');
