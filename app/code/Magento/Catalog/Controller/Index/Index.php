b8e539af60fd1904b78b0ad1a3f56ad35b8e1d64
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-02-14 17:47:43 +0530

    Add the controller

--- app/code/Magento/Catalog/Controller/Index/Index.php
+++ app/code/Magento/Catalog/Controller/Index/Index.php
@@ -20,7 +20,7 @@ class Index extends \Magento\Framework\App\Action\Action implements HttpGetActio
     public function execute()
     {
         echo "123"
-        echo "11 121 dd  2"
+        echo "11 121 dd2"
         $resultRedirect = $this->resultRedirectFactory->create();
         return $resultRedirect->setPath('/');
     }
