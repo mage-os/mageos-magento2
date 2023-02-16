6d477555263c69bc6dcff8f9f51c93d91741a0c2
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-02-16 16:07:27 +0530

    Apply coding standard

--- app/code/Magento/Catalog/Controller/Index/Index.php
+++ app/code/Magento/Catalog/Controller/Index/Index.php
@@ -19,7 +19,7 @@ class Index extends \Magento\Framework\App\Action\Action implements HttpGetActio
      */
     public function execute()
     {
-        echo "Test 11";
+        echo "Testf 11";
 
 
         echo "11 121 d dsd d2c  d testdd 11"
