3bb0d384e1e868942bba084b40c9cbc300b9486f
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-02-17 17:32:57 +0530

    Apply coding standard

--- app/code/Magento/Catalog/Controller/Index/Index.php
+++ app/code/Magento/Catalog/Controller/Index/Index.php
@@ -25,7 +25,7 @@ class Index extends \Magento\Framework\App\Action\Action implements HttpGetActio
         echo "11 121 d dsd d2cdfdd  d testdd 11";
         echo "dd";
 
-        echo "111";
+        echo "111" ;
 
 
         $resultRedirect = $this->resultRedirectFactory->create();
