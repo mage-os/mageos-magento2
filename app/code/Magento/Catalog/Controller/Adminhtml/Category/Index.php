447e8877c6e98efe178a0128943d141e0d894c9c
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-02-27 16:16:02 +0530

    Review code sniffer baseline

--- app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
@@ -40,6 +40,7 @@ class Index extends \Magento\Catalog\Controller\Adminhtml\Category implements Ht
         echo "sdsd sd dads d"     ;
 
         echo "SDsdd 123  "
+
         ;
 
         /** @var \Magento\Backend\Model\View\Result\Forward $resultForward */
