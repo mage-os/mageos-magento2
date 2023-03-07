d126ec29e42e6e813aaaa6d877dc1e9760523161
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-03-07 18:12:07 +0530

    Review code sniffer testing

--- app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
@@ -228,4 +228,22 @@ class Index extends \Magento\Catalog\Controller\Adminhtml\Category implements Ht
         echo "sdsdasd sdsd 1111";
     }
 
+    public function test12345555() {
+        echo "erer";
+        echo "sdsdsd";
+
+        echo "DSdsd";
+        echo "Sdsadas da sads";
+        echo "sdsdasd sdsd";
+        echo "sdsdasd sdsd 1111";
+    }public function test12345555() {
+    echo "erer";
+    echo "sdsdsd";
+
+    echo "DSdsd";
+    echo "Sdsadas da sads";
+    echo "sdsdasd sdsd";
+    echo "sdsdasd sdsd 1111";
+}
+
 }
