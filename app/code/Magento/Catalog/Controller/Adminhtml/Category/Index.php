f619e035249c782fafcca33552162c0b72316526
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-03-16 18:12:03 +0530

    Review code sniffer testing

--- app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
@@ -77,6 +77,10 @@ public function test123() {
         echo "1 2" 3;
         echo "21212";
     }
+public function test12344() {
+    echo "1 2" 3;
+        echo "21212";
+    }
 
 public function test12344() {
     echo "1 2" 3;
