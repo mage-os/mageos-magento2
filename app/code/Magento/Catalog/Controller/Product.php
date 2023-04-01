71aa4a203803248476d7937ed4f7eec4a97ed702
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-04-01 17:59:39 +0530

    Verify PHPCs baseline action

--- app/code/Magento/Catalog/Controller/Product.php
+++ app/code/Magento/Catalog/Controller/Product.php
@@ -29,10 +29,6 @@ abstract class Product extends \Magento\Framework\App\Action\Action implements V
         $product = $this->_objectManager->get(\Magento\Catalog\Helper\Product::class);
         return $product->initProduct($productId, $this, $params);
 
-        echo "111";
-
-        echo "12121";
-        echo "12 121";
 
 
 
