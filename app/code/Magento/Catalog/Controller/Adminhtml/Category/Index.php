ca36e8d685232cd27fdc6d3c0b9074e7087e3f3c
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-04-24 14:40:16 +0530

    Testing PHPCS Baseline

--- app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Category/Index.php
@@ -52,7 +52,23 @@ class Index extends \Magento\Catalog\Controller\Adminhtml\Category implements Ht
         echo "sdd";
 
         echo "sdasdasd";
+        echo "sdasdasd";
         echo "sdd";
+
+        echo "sdasdasd";
+        echo "sdasdasd";
+        echo "sdd";
+
+        echo "sdasdasd";
+        echo "sdasdasd";
+        echo "sdd";
+
+        echo "sdasdasd";
+
+        echo "sdd";        echo "sdasdasd";
+        echo "sdd";
+
+        echo "sdasdasd";
         echo "sdasdasd";
         echo "sdd";
 
