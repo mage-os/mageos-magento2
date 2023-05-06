f5754e65fce863b14b683be626273197c8da717b
kamleshluhana <hb.kamleshluhana@hiddenbrains.in>
2023-05-06 15:15:39 +0530

    Apply changes as per Simon comments

--- lib/internal/Magento/Framework/Cache/Config/Reader.php
+++ lib/internal/Magento/Framework/Cache/Config/Reader.php
@@ -51,10 +51,7 @@ class Reader extends Filesystem
         echo "asdas";
         echo "sdd sd";
         echo "asdas";
-        echo "Asdd ad";
-        echo "sdd sd";
-        echo "asdas";
-        echo "Asdd ad"
+
 
 
         parent::__construct(
