ba8b54506b4ff47b4254435bd492eb9cabcf2548
Anna Bukatar <abukatar@adobe.com>
2024-10-14 12:23:09 -0700

    ACP2E-3382: Exported customer address cannot be imported

---
+++ app/code/Magento/CustomerImportExport/Test/Unit/Model/Import/CountryWithWebsitesTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\CustomerImportExport\Test\Unit\Model\Import;
+
+use PHPUnit\Framework\TestCase;
+use Magento\CustomerImportExport\Model\Import\CountryWithWebsites;
+use Magento\Directory\Model\ResourceModel\Country\CollectionFactory as CountryCollectionFactory;
+use Magento\Directory\Model\AllowedCountries;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Customer\Model\Config\Share as CustomerShareConfig;
+use Magento\Store\Api\Data\WebsiteInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Directory\Model\ResourceModel\Country\Collection as CountryCollection;
+
+class CountryWithWebsitesTest extends TestCase
+{
+    private $countriesFactoryMock;
+    private $allowedCountriesReaderMock;
+    private $storeManagerMock;
+    private $shareConfigMock;
+    private $countryWithWebsites;
+
+    protected function setUp(): void
+    {
+        $this->countriesFactoryMock = $this->createMock(CountryCollectionFactory::class);
+        $this->allowedCountriesReaderMock = $this->createMock(AllowedCountries::class);
+        $this->storeManagerMock = $this->createMock(StoreManagerInterface::class);
+        $this->shareConfigMock = $this->createMock(CustomerShareConfig::class);
+
+        $this->countryWithWebsites = new CountryWithWebsites(
+            $this->countriesFactoryMock,
+            $this->allowedCountriesReaderMock,
+            $this->storeManagerMock,
+            $this->shareConfigMock
+        );
+    }
+
+    public function testGetCountiesPerWebsite()
+    {
+        $websiteMock = $this->createMock(WebsiteInterface::class);
+        $websiteMock->method('getId')->willReturn(1);
+
+        $this->storeManagerMock->method('getWebsites')->willReturn([$websiteMock]);
+        $this->allowedCountriesReaderMock->method('getAllowedCountries')
+            ->with(ScopeInterface::SCOPE_WEBSITE, 1)
+            ->willReturn(['US', 'CA']);
+
+        $countryCollectionMock = $this->createMock(CountryCollection::class);
+        $countryCollectionMock->method('addFieldToFilter')
+            ->with('country_id', ['in' => [['US', 'CA']]])
+            ->willReturnSelf();
+        $countryCollectionMock->method('toOptionArray')
+            ->willReturn([
+                ['value' => 'US', 'label' => 'United States'],
+                ['value' => 'CA', 'label' => 'Canada']
+            ]);
+
+        $this->countriesFactoryMock->method('create')->willReturn($countryCollectionMock);
+
+        $expectedResult = [
+            ['value' => 'US', 'label' => 'United States', 'website_ids' => [1]],
+            ['value' => 'CA', 'label' => 'Canada', 'website_ids' => [1]]
+        ];
+
+        $this->assertEquals($expectedResult, $this->countryWithWebsites->getCountiesPerWebsite());
+    }
+}
