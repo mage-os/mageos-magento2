0f589e0b0ad862627ba0e17a1b3fe55b9147e8da
lakshmana49 <glo28218@adobe.com>
2024-10-19 00:40:29 +0530

    ACP2E-3350: Disabled Categories are no longer have their names grayed out in the category tree

--- dev/tests/integration/testsuite/Magento/Catalog/Block/Adminhtml/Category/TreeBlockTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Block/Adminhtml/Category/TreeBlockTest.php
@@ -11,6 +11,8 @@ use Magento\Catalog\Test\Fixture\Category as CategoryFixture;
 use Magento\Framework\View\LayoutInterface;
 use Magento\TestFramework\Fixture\AppArea;
 use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorage;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\TestCase\AbstractBackendController;
 
 /**
@@ -20,19 +22,33 @@ class TreeBlockTest extends AbstractBackendController
 {
 
     /**
+     * @var DataFixtureStorage
+     */
+    private $fixtures;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fixtures = $this->_objectManager->get(DataFixtureStorageManager::class)->getStorage();
+    }
+
+    /**
      * Test disabled categories in the tree block.
      */
     #[
         AppArea('adminhtml'),
-        DataFixture(CategoryFixture::class, ['is_active' => false], as:'new_category'),
+        DataFixture(CategoryFixture::class, as: 'c10'),
+        DataFixture(CategoryFixture::class, ['parent_id' => '$c10.id$', 'is_active' => false], 'c11'),
     ]
     public function testDisabledCategoriesHtml()
     {
-        $this->dispatch("backend/catalog/category/index/");
-        $this->assertEquals(200, $this->getResponse()->getHttpResponseCode());
+        $category = $this->fixtures->get('c10');
         $layout = $this->_objectManager->get(LayoutInterface::class);
-        $categoryTree = $layout->getBlock('category.tree');
-        $blockHtml = $categoryTree->toHtml();
-        $this->assertStringContainsString('no-active-category', $blockHtml);
+        $categoryTreeBlock = $layout->createBlock(Tree::class);
+        $categoryTreeArray = $categoryTreeBlock->getTree($category);
+        $this->assertCount(1, $categoryTreeArray);
+        $this->assertArrayHasKey('a_attr', $categoryTreeArray[0]);
+        $this->assertArrayHasKey('class', $categoryTreeArray[0]['a_attr']);
+        $this->assertStringContainsString('no-active-category', $categoryTreeArray[0]['a_attr']['class']);
     }
 }
