844eee1f3c8cb6e00a44245685952fedca7e2e41
Rajesh Kumar <glo71317@adobe.com>
2024-10-21 18:19:15 +0530

    AC-13209::Investigate the latest version of web-token/jwt-framework

--- setup/src/Magento/Setup/Test/Unit/Console/Style/TestOutput.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Style/TestOutput.php
@@ -25,7 +25,7 @@ class TestOutput extends Output
      * @param string $message
      * @param bool $newline
      */
-    protected function doWrite($message, $newline)
+    protected function doWrite($message, $newline): void
     {
         $this->output .= $message . ($newline ? "\n" : '');
     }
