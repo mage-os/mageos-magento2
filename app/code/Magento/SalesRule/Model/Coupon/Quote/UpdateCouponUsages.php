a60f8abe8e37218d8a6f7231fc0d348054c49c24
Manohar Prabhutendolkar <eze25057@adobe.com>
2024-10-24 09:34:43 +0530

    AC-12802: coupon usage increment validation

--- app/code/Magento/SalesRule/Model/Coupon/Quote/UpdateCouponUsages.php
+++ app/code/Magento/SalesRule/Model/Coupon/Quote/UpdateCouponUsages.php
@@ -70,8 +70,8 @@ class UpdateCouponUsages
         $updateInfo->setCustomerId((int)$quote->getCustomerId());
         $updateInfo->setIsIncrement($increment);
 
-        $this->couponUsagePublisher->publish($updateInfo);
-        $this->processor->updateCustomerRulesUsages($updateInfo);
         $this->processor->updateCouponUsages($updateInfo);
+        $this->processor->updateCustomerRulesUsages($updateInfo);
+        $this->couponUsagePublisher->publish($updateInfo);
     }
 }
