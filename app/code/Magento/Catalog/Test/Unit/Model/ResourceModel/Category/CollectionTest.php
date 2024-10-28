1a8201cf4ac6f50ebe962f3af69da5d997efeb87
Oleksandr Iegorov <oiegorov@adobe.com>
2024-10-28 15:55:17 -0500

    ACP2E-3389: Adobe Commerce backend loads categories very slowly

--- app/code/Magento/Catalog/Test/Unit/Model/ResourceModel/Category/CollectionTest.php
+++ app/code/Magento/Catalog/Test/Unit/Model/ResourceModel/Category/CollectionTest.php
@@ -27,6 +27,10 @@ use Magento\Catalog\Model\ResourceModel\Category as CategoryEntity;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @SuppressWarnings(PHPMD.TooManyFields)
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class CollectionTest extends TestCase
 {
 
