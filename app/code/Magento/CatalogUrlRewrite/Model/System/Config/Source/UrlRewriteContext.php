fde0f97b9fcf03527d0fe0f9e12e05e64d423082
Alexandru Plapana <aplapana@adobe.com>
2024-11-04 12:45:46 +0200

    ACP2E-3383: Incorrect url_rewrite entries creation with multiple stores in one store group

---
+++ app/code/Magento/CatalogUrlRewrite/Model/System/Config/Source/UrlRewriteContext.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2024 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogUrlRewrite\Model\System\Config\Source;
+
+use Magento\CatalogUrlRewrite\Model\ProductScopeRewriteGenerator;
+
+class UrlRewriteContext implements \Magento\Framework\Data\OptionSourceInterface
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => ProductScopeRewriteGenerator::WEBSITE_URL_REWRITE_SCOPE, 'label' => __('Website')],
+            ['value' => ProductScopeRewriteGenerator::STORE_VIEW_URL_REWRITE_SCOPE, 'label' => __('Store View')]
+        ];
+    }
+}
