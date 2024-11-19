16b777594d7f2ed3a48a062716edbde22dc19508
jayrangnani-gl <142883278+jayrangnani-gl@users.noreply.github.com>
2024-11-19 11:43:03 +0530

    AC-13182::[Upgrade Build] Manual Upgrade Build 2.4.7-p2 -> 2.4.8-beta1-Revert Changes

--- lib/internal/Magento/Framework/Mview/Config/Converter.php
+++ lib/internal/Magento/Framework/Mview/Config/Converter.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * Copyright 2014 Adobe
- * All Rights Reserved.
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
 namespace Magento\Framework\Mview\Config;
 
 use Magento\Framework\Mview\View\AdditionalColumnsProcessor\DefaultProcessor;
 use Magento\Framework\Mview\View\ChangelogBatchWalker;
 use Magento\Framework\Mview\View\SubscriptionInterface;
-use Magento\Framework\App\ResourceConnection;
 
 class Converter implements \Magento\Framework\Config\ConverterInterface
 {
@@ -23,21 +22,13 @@ class Converter implements \Magento\Framework\Config\ConverterInterface
     private $defaultIterator;
 
     /**
-     * @var ResourceConnection
-     */
-    private $resourceConnection;
-
-    /**
-     * @param ResourceConnection $resourceConnection
      * @param string $defaultProcessor
      * @param string $defaultIterator
      */
     public function __construct(
-        ResourceConnection $resourceConnection,
         string $defaultProcessor = DefaultProcessor::class,
         string $defaultIterator = ChangelogBatchWalker::class
     ) {
-        $this->resourceConnection = $resourceConnection;
         $this->defaultProcessor = $defaultProcessor;
         $this->defaultIterator = $defaultIterator;
     }
@@ -108,15 +99,7 @@ class Converter implements \Magento\Framework\Config\ConverterInterface
                         continue;
                     }
                     $name = $this->getAttributeValue($subscription, 'name');
-                    $configColumn = $this->getAttributeValue($subscription, 'entity_column');
-                    $column = $this->checkifcolumnexist($name, $configColumn);
-
-                    if (empty($column)) {
-                        throw new \InvalidArgumentException(
-                            'Column ' . $configColumn . ' does not exist in table ' . $name
-                        );
-                    }
-
+                    $column = $this->getAttributeValue($subscription, 'entity_column');
                     $subscriptionModel = $this->getAttributeValue($subscription, 'subscription_model');
 
                     if (!empty($subscriptionModel)
@@ -171,24 +154,5 @@ class Converter implements \Magento\Framework\Config\ConverterInterface
         }
 
         return $additionalColumns;
-    }
-
-    /**
-     * Check if column exists in table, otherwise return primary key column
-     *
-     * @param string $tableName
-     * @param string $columnName
-     * @return string
-     */
-    public function checkifcolumnexist($tableName, $columnName) : string
-    {
-        $connection = $this->resourceConnection->getConnection();
-        $tableName = $this->resourceConnection->getTableName($tableName);
-
-        if (!$connection->isTableExists($tableName) || $connection->tableColumnExists($tableName, $columnName)) {
-            return $columnName;
-        }
-
-        return '';
     }
 }
--- lib/internal/Magento/Framework/Mview/Test/Unit/Config/ConverterTest.php
+++ lib/internal/Magento/Framework/Mview/Test/Unit/Config/ConverterTest.php
@@ -1,14 +1,12 @@
 <?php
 /**
- * Copyright 2015 Adobe
- * All Rights Reserved.
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
 declare(strict_types=1);
 
 namespace Magento\Framework\Mview\Test\Unit\Config;
 
-use Magento\Framework\App\ResourceConnection;
-use Magento\Framework\DB\Adapter\AdapterInterface;
 use Magento\Framework\Mview\Config\Converter;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -20,27 +18,13 @@ class ConverterTest extends TestCase
      */
     protected $_model;
 
-    /**
-     * @var ResourceConnection|MockObject
-     */
-    protected $resourceConnectionMock;
-
-    /**
-     * @var \Magento\Framework\DB\Adapter\AdapterInterface|MockObject
-     */
-    protected $connectionMock;
-
     protected function setUp(): void
     {
-        $this->connectionMock = $this->createMock(AdapterInterface::class);
-        $this->resourceConnectionMock = $this->createMock(ResourceConnection::class);
-        $this->_model = new Converter($this->resourceConnectionMock);
+        $this->_model = new Converter();
     }
 
     public function testConvert()
     {
-        $this->resourceConnectionMock->method('getConnection')
-            ->willReturn($this->connectionMock);
         $data = include __DIR__ . '/../_files/mview_config.php';
         $dom = new \DOMDocument();
         $dom->loadXML($data['inputXML']);
