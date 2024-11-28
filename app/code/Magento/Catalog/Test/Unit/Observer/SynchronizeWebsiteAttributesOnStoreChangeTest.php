e1344916ae643ca13cdda074b4b2a97957621298
Pawan.kumar <pawan.kumar9@globallogic.com>
2024-11-28 16:01:16 +0530

    cia-2.4.8-beta2-develop-2.4-develop-sync-11222024: Fix static tests

--- app/code/Magento/Catalog/Test/Unit/Observer/SynchronizeWebsiteAttributesOnStoreChangeTest.php
+++ app/code/Magento/Catalog/Test/Unit/Observer/SynchronizeWebsiteAttributesOnStoreChangeTest.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
+ * Copyright Adobe 2024
+ * All rights reserved.
  * See COPYING.txt for license details.
  */
 declare(strict_types=1);
