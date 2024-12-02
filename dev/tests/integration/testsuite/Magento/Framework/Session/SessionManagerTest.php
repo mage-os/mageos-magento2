c5ffcf041d42299bb602417a1ba893922e65f9e5
Bhavin Parmar <cod62627@adobe.com>
2024-12-02 12:26:25 +0530

    AC-13416:: session_set_save_handler() with more than 2 arguments deprecated in PHP 8.4

--- dev/tests/integration/testsuite/Magento/Framework/Session/SessionManagerTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Session/SessionManagerTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright 2012 Adobe
- * All Rights Reserved.
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
 // @codingStandardsIgnoreStart
 namespace {
