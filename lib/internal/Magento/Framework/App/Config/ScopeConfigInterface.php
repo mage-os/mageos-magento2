cf6e875a1add46ba44f27c2c7c42b723cfdde2f8
Dimitri BOUTEILLE <dimitri.bouteille@reflet-digital.com>
2024-12-16 16:19:44 +0100

    Fix review

--- lib/internal/Magento/Framework/App/Config/ScopeConfigInterface.php
+++ lib/internal/Magento/Framework/App/Config/ScopeConfigInterface.php
@@ -22,7 +22,7 @@ interface ScopeConfigInterface
      *
      * @param string $path The path through the tree of configuration values, e.g., 'general/store_information/name'
      * @param string $scopeType The scope to use to determine config value, e.g., 'store' or 'default'
-     * @param null|int|string|\Magento\Store\Api\Data\StoreInterface $scopeCode
+     * @param null|int|string|\Magento\Framework\App\ScopeInterface $scopeCode
      * @return mixed
      */
     public function getValue($path, $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT, $scopeCode = null);
@@ -32,7 +32,7 @@ interface ScopeConfigInterface
      *
      * @param string $path The path through the tree of configuration values, e.g., 'general/store_information/name'
      * @param string $scopeType The scope to use to determine config value, e.g., 'store' or 'default'
-     * @param null|int|string|\Magento\Store\Api\Data\StoreInterface $scopeCode
+     * @param null|int|string|\Magento\Framework\App\ScopeInterface $scopeCode
      * @return bool
      */
     public function isSetFlag($path, $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT, $scopeCode = null);
