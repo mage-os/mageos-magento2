3931e78f950adaff0245e4aecd501f577ee95d9f
Olga Moyseyenko <moyseyen@adobe.com>
2024-12-24 17:45:00 -0600

    ACP2E-3501: VAPT: Business Logic Error - future date as customer date of birth

--- app/code/Magento/Customer/Model/Validator/Dob.php
+++ app/code/Magento/Customer/Model/Validator/Dob.php
@@ -7,10 +7,9 @@ declare(strict_types=1);
 
 namespace Magento\Customer\Model\Validator;
 
+use DateTimeZone;
 use Magento\Customer\Model\Customer;
 use Magento\Framework\Validator\AbstractValidator;
-use Magento\Store\Api\Data\StoreInterface as StoreInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
 /**
  * Customer dob field validator.
@@ -18,66 +17,42 @@ use Magento\Store\Model\StoreManagerInterface;
 class Dob extends AbstractValidator
 {
     /**
-     * @var \DateTime
-     */
-    private \DateTime $currentDate;
-
-   /**
-    * @var StoreManagerInterface
-    */
-    private StoreManagerInterface $storeManager;
-
-    /**
-     * @param StoreManagerInterface $storeManager
-     */
-    public function __construct(StoreManagerInterface $storeManager)
-    {
-        $this->currentDate = new \DateTime();
-        $this->storeManager = $storeManager;
-    }
-
-    /**
-     * Validate name fields.
+     * Validate dob field.
      *
      * @param Customer $customer
      * @return bool
      */
     public function isValid($customer): bool
     {
-        if (!$this->isValidDob($customer->getDob(), $customer->getStoreId())) {
-            parent::_addMessages([['dob' => 'The Date of Birth should not be greater than today.']]);
+        $timezone = new DateTimeZone($customer->getStore()->getConfig('general/locale/timezone'));
+        if (!$this->isValidDob($customer->getDob(), $timezone)) {
+            $this->_addMessages([['dob' => 'The Date of Birth should not be greater than today.']]);
         }
 
-        return count($this->_messages) == 0;
+        return count($this->_messages) === 0;
     }
 
     /**
      * Check if specified dob is not in the future
      *
      * @param string|null $dobValue
-     * @param null|string|bool|int|StoreInterface $storeId
+     * @param DateTimeZone $timezone
      * @return bool
      */
-    private function isValidDob(?string $dobValue, null|string|bool|int|StoreInterface $storeId): bool
+    private function isValidDob(?string $dobValue, ?DateTimeZone $timezone = null): bool
     {
         if ($dobValue != null) {
 
-            // Get the timezone of the store
-            $store = $this->storeManager->getStore($storeId);
-            $timezone = $store->getConfig('general/locale/timezone');
-
             // Get the date of birth and set the time to 00:00:00
-            $dobDate = new \DateTime($dobValue, new \DateTimeZone($timezone));
+            $dobDate = new \DateTime($dobValue, $timezone);
             $dobDate->setTime(0, 0, 0);
 
             // Get the timestamp of the date of birth and the current date
             $dobTimestamp = $dobDate->getTimestamp();
-            $currentTimestamp = $this->currentDate->getTimestamp();
+            $currentTimestamp = time();
 
             // If the date's of birth first minute is in the future, return false - the day has not started yet
-            if ($dobTimestamp > $currentTimestamp) {
-                return false;
-            }
+            return ($dobTimestamp <= $currentTimestamp);
         }
 
         return true;
