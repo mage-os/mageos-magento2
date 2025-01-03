dfbc2a2543970c796ec260bfd4fef695e60420ea
Bhavin Parmar <cod62627@adobe.com>
2025-01-03 13:12:40 +0530

    AC-13306: [CE] Adobe Commerce 2.4.8 core code is compatible with PHP 8.4

--- app/code/Magento/Catalog/Model/AbstractModel.php
+++ app/code/Magento/Catalog/Model/AbstractModel.php
@@ -9,6 +9,9 @@ use Magento\Framework\Api\AttributeValueFactory;
 
 /**
  * Abstract model for catalog entities
+ *
+ * phpcs:disable Magento2.Classes.AbstractApi
+ * @api
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  * @author      Magento Core Team <core@magentocommerce.com>
  * @since 100.0.2
