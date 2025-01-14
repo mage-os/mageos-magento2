c0312e153a64e274d4f1d8bf2365335a6b139404
Abhishek Pathak <wip44850@adobe.com>
2025-01-14 13:54:09 +0530

    AC-7917: Order item should contain parent sku, to enable reorder functionality

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Sales/RetrieveOrdersWithConfigurableProductByOrderNumberTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Sales/RetrieveOrdersWithConfigurableProductByOrderNumberTest.php
@@ -7,8 +7,22 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Sales;
 
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\ConfigurableProduct\Test\Fixture\AddProductToCart as AddConfigurableProductToCartFixture;
+use Magento\NegotiableQuote\Test\Fixture\QuoteIdMask;
+use Magento\Checkout\Test\Fixture\PlaceOrder as PlaceOrderFixture;
+use Magento\Checkout\Test\Fixture\SetBillingAddress as SetBillingAddressFixture;
+use Magento\Checkout\Test\Fixture\SetDeliveryMethod as SetDeliveryMethodFixture;
+use Magento\Checkout\Test\Fixture\SetPaymentMethod as SetPaymentMethodFixture;
+use Magento\Checkout\Test\Fixture\SetShippingAddress as SetShippingAddressFixture;
+use Magento\ConfigurableProduct\Test\Fixture\Attribute as AttributeFixture;
+use Magento\ConfigurableProduct\Test\Fixture\Product as ConfigurableProductFixture;
+use Magento\Customer\Test\Fixture\Customer;
 use Magento\Framework\Exception\AuthenticationException;
 use Magento\GraphQl\GetCustomerAuthenticationHeader;
+use Magento\Quote\Test\Fixture\CustomerCart;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
 
@@ -29,31 +43,59 @@ class RetrieveOrdersWithConfigurableProductByOrderNumberTest extends GraphQlAbst
         $this->customerAuthenticationHeader = $objectManager->get(GetCustomerAuthenticationHeader::class);
     }
 
-    /**
-     * Test customer order details with configurable product with child items
-     *
-     * @magentoApiDataFixture Magento/Customer/_files/customer.php
-     * @magentoApiDataFixture Magento/Sales/_files/customer_order_with_configurable_product.php
-     */
+    #[
+        DataFixture(Customer::class, ['email' => 'customer@example.com'], as: 'customer'),
+        DataFixture(ProductFixture::class, as: 'product'),
+        DataFixture(AttributeFixture::class, as: 'attribute'),
+        DataFixture(
+            ConfigurableProductFixture::class,
+            ['_options' => ['$attribute$'], '_links' => ['$product$']],
+            'configurable_product'
+        ),
+        DataFixture(
+            CustomerCart::class,
+            [
+                'customer_id' => '$customer.id$'
+            ],
+            'quote'
+        ),
+        DataFixture(QuoteIdMask::class, ['cart_id' => '$quote.id$'], 'quoteIdMask'),
+        DataFixture(
+            AddConfigurableProductToCartFixture::class,
+            [
+                'cart_id' => '$quote.id$',
+                'product_id' => '$configurable_product.id$',
+                'child_product_id' => '$product.id$',
+                'qty' => 1
+            ],
+        ),
+        DataFixture(SetBillingAddressFixture::class, ['cart_id' => '$quote.id$']),
+        DataFixture(SetShippingAddressFixture::class, ['cart_id' => '$quote.id$']),
+        DataFixture(SetDeliveryMethodFixture::class, ['cart_id' => '$quote.id$']),
+        DataFixture(SetPaymentMethodFixture::class, ['cart_id' => '$quote.id$']),
+        DataFixture(PlaceOrderFixture::class, ['cart_id' => '$quote.id$'], 'order'),
+    ]
     public function testGetCustomerOrderConfigurableProduct(): void
     {
-        $orderNumber = '100000001';
+        $order = DataFixtureStorageManager::getStorage()->get('order');
+        $orderNumber = $order->getIncrementId();
+        $product = DataFixtureStorageManager::getStorage()->get('product');
+        $configurableProduct = DataFixtureStorageManager::getStorage()->get('configurable_product');
         $customerOrderResponse = $this->getCustomerOrderQueryConfigurableProduct($orderNumber);
         $customerOrderItems = $customerOrderResponse[0];
-        $this->assertEquals('Pending Payment', $customerOrderItems['status']);
         $configurableItemInTheOrder = $customerOrderItems['items'][0];
         $this->assertEquals(
-            'simple_10',
+            $product->getSku(),
             $configurableItemInTheOrder['product_sku']
         );
 
         $expectedConfigurableOptions = [
             '__typename' => 'ConfigurableOrderItem',
-            'product_sku' => 'simple_10',
-            'product_name' => 'Configurable Product',
-            'parent_sku' => 'configurable',
-            'product_url_key' => 'configurable-product',
-            'quantity_ordered' => 2
+            'product_sku' => $product->getSku(),
+            'product_name' => $configurableProduct->getName(),
+            'parent_sku' => $configurableProduct->getSku(),
+            'product_url_key' => $configurableProduct->getUrlKey(),
+            'quantity_ordered' => 1
         ];
         $this->assertEquals($expectedConfigurableOptions, $configurableItemInTheOrder);
     }
