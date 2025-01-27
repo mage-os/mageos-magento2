54736bf5abcab751879fc932bdb55a5dbe197814
Alexandru Plapana <aplapana@adobe.com>
2025-01-27 16:30:49 +0200

    ACP2E-3618: [CLOUD] Re-order button functionality

--- app/code/Magento/CatalogInventory/Model/StockStateProvider.php
+++ app/code/Magento/CatalogInventory/Model/StockStateProvider.php
@@ -159,7 +159,7 @@ class StockStateProvider implements StockStateProviderInterface
         if (!$stockItem->getIsInStock()) {
             $result->setHasError(true)
                 ->setErrorCode('out_stock')
-                ->setMessage(__('The product with SKU %sku is out of stock.', ['sku' => $item->getSku()]))
+                ->setMessage(__('Product %name is out of stock.', ['sku' => $stockItem->getProductName()]))
                 ->setQuoteMessage(__('Some of the products are out of stock.'))
                 ->setQuoteMessageIndex('stock');
             $result->setItemUseOldQty(true);
