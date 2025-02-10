04b05593e4e35588ad7b20ad39d24ae259cd9286
Alexandru Plapana <aplapana@adobe.com>
2025-02-10 12:16:44 +0200

    ACP2E-3660: REST endpoint Product Import Json not validate the mandatory fields

---
+++ app/code/Magento/CatalogImportExport/Model/Import/Product/Validator/Name.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogImportExport\Model\Import\Product\Validator;
+
+use Magento\CatalogImportExport\Model\Import\Product\RowValidatorInterface;
+
+class Name extends AbstractImportValidator implements RowValidatorInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function isValid($value)
+    {
+        $this->_clearMessages();
+        if (empty($value['name']) && $value['name'] !== $this->context->getEmptyAttributeValueConstant()) {
+            $this->_addMessages(
+                [
+                    sprintf(
+                        $this->context->retrieveMessageTemplate(self::ERROR_INVALID_ATTRIBUTE_TYPE),
+                        'name',
+                        'not empty'
+                    )
+                ]
+            );
+            return false;
+        }
+        return true;
+    }
+}
