b68d8b749a12fdb25f48c6396ff91e31a82bda49
engcom-Charlie <engcom-vendorworker-charlie@adobe.com>
2025-02-12 15:16:43 +0530

    After revert, fixed some code base to avoid static tests failures

--- lib/internal/Magento/Framework/Message/Manager.php
+++ lib/internal/Magento/Framework/Message/Manager.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2017 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Framework\Message;
 
@@ -21,7 +21,7 @@ class Manager implements ManagerInterface
     /**
      * Default message group
      */
-    const DEFAULT_GROUP = 'default';
+    public const DEFAULT_GROUP = 'default';
 
     /**
      * @var Session
