653be88d50eeb3d7cd60e8363b1c409ddaf9ec33
Sokly Meach <meach@adobe.com>
2025-02-26 15:29:14 -0600

    ACP2E-3709: [Cloud] Changing table column type from Int to Decimal using db_schema.xml file In Magento 2 Results In Errors

--- lib/internal/Magento/Framework/Config/Dom.php
+++ lib/internal/Magento/Framework/Config/Dom.php
@@ -171,10 +171,11 @@ class Dom
         /* Update matched node attributes and value */
         if ($matchedNode) {
             //different node type
-            if ($this->typeAttributeName &&
+            if (($this->typeAttributeName &&
                 $node->hasAttribute($this->typeAttributeName) &&
                 $matchedNode->hasAttribute($this->typeAttributeName) &&
-                $node->getAttribute($this->typeAttributeName) !== $matchedNode->getAttribute($this->typeAttributeName)
+                $node->getAttribute($this->typeAttributeName) !== $matchedNode->getAttribute($this->typeAttributeName))
+                || ($node->tagName === 'column')
             ) {
                 $parentMatchedNode = $this->_getMatchedNode($parentPath);
                 $newNode = $this->dom->importNode($node, true);
