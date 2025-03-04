c1e648f71db276843c855456e53f7f6184614c36
Alexandru Plapana <aplapana@adobe.com>
2025-03-04 13:14:19 +0200

    ACP2E-3449: Slow query is executed when product widget is included via pagebuilder

--- dev/tests/integration/testsuite/Magento/Rule/Model/Condition/Sql/BuilderTest.php
+++ dev/tests/integration/testsuite/Magento/Rule/Model/Condition/Sql/BuilderTest.php
@@ -8,6 +8,7 @@ namespace Magento\Rule\Model\Condition\Sql;
 
 use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
 use Magento\Eav\Test\Fixture\AttributeOption as AttributeOptionFixture;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Fixture\DataFixture;
 use Magento\Catalog\Test\Fixture\MultiselectAttribute;
@@ -15,17 +16,21 @@ use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductColl
 use Magento\CatalogWidget\Model\RuleFactory;
 use Magento\CatalogWidget\Model\Rule\Condition\Combine as CombineCondition;
 use Magento\CatalogWidget\Model\Rule\Condition\Product as ProductCondition;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Test for Magento\Rule\Model\Condition\Sql\Builder
  */
-class BuilderTest extends \PHPUnit\Framework\TestCase
+class BuilderTest extends TestCase
 {
     /**
      * @var Builder
      */
-    private $model;
+    private Builder $model;
 
+    /**
+     * @inheritDoc
+     */
     protected function setUp(): void
     {
         $this->model = Bootstrap::getObjectManager()->create(Builder::class);
@@ -33,9 +38,11 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @param array $conditions
-     * @param string $expectedSql
-     * @dataProvider attachConditionToCollectionDataProvider
+     * @param string $expectedWhere
+     * @param string $expectedOrder
      * @return void
+     * @throws LocalizedException
+     * @dataProvider attachConditionToCollectionDataProvider
      */
     #[
         DataFixture(MultiselectAttribute::class, ['attribute_code' => 'multi_select_attr'], 'multiselect'),
@@ -59,30 +66,31 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
             'multiselect_custom_attribute_option_2'
         )
     ]
-    public function testAttachConditionToCollection(array $conditions, string $expectedSql): void
-    {
+    public function testAttachConditionToCollection(
+        array $conditions,
+        string $expectedWhere,
+        string $expectedOrder
+    ): void {
         /** @var ProductCollectionFactory $collectionFactory */
         $collectionFactory = Bootstrap::getObjectManager()->create(ProductCollectionFactory::class);
         $collection = $collectionFactory->create();
-        /*$multiselect = DataFixtureStorageManager::getStorage()->get(
-            'multi_select_attr'
-        );*/
-        $attribute = Bootstrap::getObjectManager()->create(
-            Attribute::class
-        );
-        $attribute->load('multi_select_attr', 'attribute_code');
-        $multiselectAttributeOptionIds = [];
-        foreach ($attribute->getOptions() as $option) {
-            if ($option->getValue()) {
-                $multiselectAttributeOptionIds[] = $option->getValue();
+        foreach ($conditions as $key => $condition) {
+            if (isset($condition['attribute']) && $condition['attribute'] === 'multiselect_attribute') {
+                $multiselect = Bootstrap::getObjectManager()->create(
+                    Attribute::class
+                );
+                $multiselect->load('multi_select_attr', 'attribute_code');
+                $multiselectAttributeOptionIds = [];
+                foreach ($multiselect->getOptions() as $option) {
+                    if ($option->getValue()) {
+                        $multiselectAttributeOptionIds[] = $option->getValue();
+                    }
+                }
+
+                $condition[$key]['value'] = implode(',', $multiselectAttributeOptionIds);
             }
         }
-        $conditions['1--4'] = [
-            'type' => ProductCondition::class,
-            'attribute' => 'multiselect_attribute',
-            'operator' => '{}',
-            'value' => implode(',', $multiselectAttributeOptionIds),
-        ];
+
         /** @var RuleFactory $ruleFactory */
         $ruleFactory = Bootstrap::getObjectManager()->create(RuleFactory::class);
         $rule = $ruleFactory->create();
@@ -94,10 +102,8 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
         $rule->loadPost($ruleConditionArray);
         $this->model->attachConditionToCollection($collection, $rule->getConditions());
 
-        $whereString = "/\(category_id IN \('3'\).+\(`e`\.`entity_id` = '2017-09-15 00:00:00'\)"
-            .".+\(`e`\.`sku` IN \('sku1', 'sku2', 'sku3', 'sku4', 'sku5'\)"
-            . ".+ORDER BY \(FIELD\(`e`.`sku`, 'sku1', 'sku2', 'sku3', 'sku4', 'sku5'\)\)/";
-        $this->assertStringContainsString($expectedSql, $collection->getSelectSql(true));
+        $this->assertStringContainsString($expectedWhere, $collection->getSelectSql(true));
+        $this->assertStringContainsString($expectedOrder, $collection->getSelectSql(true));
     }
 
     /**
@@ -133,7 +139,43 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
                         'value' => 'sku1,sku2,sku3,sku4,sku5',
                     ]
                 ],
-                "WHERE ((((`e`.`entity_id` IN (SELECT `catalog_category_product`.`product_id` FROM `catalog_category_product` WHERE (category_id IN ('3')))) AND(`e`.`entity_id` = '2017-09-15 00:00:00') AND(`e`.`sku` IN ('sku1', 'sku2', 'sku3', 'sku4', 'sku5')) ))) AND (e.created_in <= 1) AND (e.updated_in > 1) ORDER BY (FIELD(`e`.`sku`, 'sku1', 'sku2', 'sku3', 'sku4', 'sku5'))"
+                "WHERE ((((`e`.`entity_id` IN (SELECT `catalog_category_product`.`product_id` FROM " .
+                "`catalog_category_product` WHERE (category_id IN ('3')))) AND(`e`.`entity_id` = '2017-09-15 00:00:00')"
+                ." AND(`e`.`sku` IN ('sku1', 'sku2', 'sku3', 'sku4', 'sku5')) ))) AND (e.created_in <= 1) AND " .
+                "(e.updated_in > 1) ",
+                "ORDER BY (FIELD(`e`.`sku`, 'sku1', 'sku2', 'sku3', 'sku4', 'sku5'))"
+            ],
+            [
+                [
+                    '1' => [
+                        'type' => CombineCondition::class,
+                        'aggregator' => 'all',
+                        'value' => '1',
+                        'new_child' => '',
+                    ],
+                    '1--1' => [
+                        'type' => ProductCondition::class,
+                        'attribute' => 'category_ids',
+                        'operator' => '==',
+                        'value' => '3',
+                    ],
+                    '1--2' => [
+                        'type' => ProductCondition::class,
+                        'attribute' => 'sku',
+                        'operator' => '()',
+                        'value' => 'sku1,sku2,sku3',
+                    ],
+                    '1--3' => [
+                        'type' => ProductCondition::class,
+                        'attribute' => 'multiselect_attribute',
+                        'operator' => '{}',
+                    ]
+                ],
+                "WHERE ((((`e`.`entity_id` IN (SELECT `catalog_category_product`.`product_id` FROM " .
+                "`catalog_category_product` WHERE (category_id IN ('3')))) AND(`e`.`sku` IN " .
+                "('sku1', 'sku2', 'sku3')) AND(`multiselect_attribute` LIKE '%%') ))) AND " .
+                "(e.created_in <= 1) AND (e.updated_in > 1) ",
+                "ORDER BY (FIELD(`e`.`sku`, 'sku1', 'sku2', 'sku3'))"
             ]
         ];
     }
