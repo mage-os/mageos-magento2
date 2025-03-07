956171bc4d1118707605e61b681b579b36d98b44
Sokly Meach <meach@adobe.com>
2025-03-07 14:26:24 -0600

    ACP2E-3667: [Mainline ]Fedex International Priority unavailable.

--- dev/tests/integration/testsuite/Magento/Fedex/Model/CarrierTest.php
+++ dev/tests/integration/testsuite/Magento/Fedex/Model/CarrierTest.php
@@ -38,7 +38,7 @@ class CarrierTest extends \PHPUnit\Framework\TestCase
     public static function getCodeDataProvider()
     {
         return [
-            ['method', 21],
+            ['method', 30],
             ['dropoff', 5],
             ['packaging', 7],
             ['containers_filter', 4],
