961945469e5509cadcc610141fb76c449c291fa4
engcom-Hotel <engcom-vendorworker-hotel@adobe.com>
2025-03-10 20:27:21 +0530

    Removed some file and unused code

--- app/code/Magento/Customer/Model/Form.php
+++ app/code/Magento/Customer/Model/Form.php
@@ -15,12 +15,7 @@ class Form extends \Magento\Eav\Model\Form
      * XML configuration paths for "Disable autocomplete on storefront" property
      */
     public const XML_PATH_ENABLE_AUTOCOMPLETE = 'customer/password/autocomplete_on_storefront';
-
-    /**
-     * XML configuration paths for "Enable CAPTCHA on Storefront" property
-     */
-    public const XML_PATH_CUSTOMER_CAPTCHA_ENABLED = 'customer/captcha/enable';
-
+    
     /**
      * Current module pathname
      *
