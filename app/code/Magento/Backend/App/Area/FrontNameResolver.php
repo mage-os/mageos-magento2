5535218752c6cbd366065b383207603ec07071c8
glo24157 <glo24157@adobe.com>
2025-03-14 19:31:09 +0530

    MCLOUD-13500 - Unable to open backend with 2.4.8-beta134 in cloud instance
    Fix regression issue

--- app/code/Magento/Backend/App/Area/FrontNameResolver.php
+++ app/code/Magento/Backend/App/Area/FrontNameResolver.php
@@ -128,10 +128,11 @@ class FrontNameResolver implements FrontNameResolverInterface
         if ($this->scopeConfig->getValue(self::XML_PATH_USE_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE)) {
             $backendUrl = $this->scopeConfig->getValue(self::XML_PATH_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE);
         } else {
-            $backendUrl = $this->config->getValue(Store::XML_PATH_UNSECURE_BASE_URL);
+            $xmlPath = $this->request->isSecure() ? Store::XML_PATH_SECURE_BASE_URL : Store::XML_PATH_UNSECURE_BASE_URL;
+            $backendUrl = $this->config->getValue($xmlPath);
             if ($backendUrl === null) {
                 $backendUrl = $this->scopeConfig->getValue(
-                    Store::XML_PATH_UNSECURE_BASE_URL,
+                    $xmlPath,
                     ScopeInterface::SCOPE_STORE
                 );
             }
