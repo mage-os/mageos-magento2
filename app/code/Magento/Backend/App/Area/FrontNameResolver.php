7a81758f00d9acbad8b42e0420f9da6ed7bf602d
glo24157 <glo24157@adobe.com>
2025-03-14 21:49:17 +0530

    MCLOUD-13500 - Unable to open backend with 2.4.8-beta134 in cloud instance
    Fix static test failures

--- app/code/Magento/Backend/App/Area/FrontNameResolver.php
+++ app/code/Magento/Backend/App/Area/FrontNameResolver.php
@@ -119,24 +119,14 @@ class FrontNameResolver implements FrontNameResolverInterface
      *
      * @return bool
      */
-    public function isHostBackend()
+    public function isHostBackend(): bool
     {
         if (!$this->request->getServer('HTTP_HOST')) {
             return false;
         }
 
-        if ($this->scopeConfig->getValue(self::XML_PATH_USE_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE)) {
-            $backendUrl = $this->scopeConfig->getValue(self::XML_PATH_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE);
-        } else {
-            $xmlPath = $this->request->isSecure() ? Store::XML_PATH_SECURE_BASE_URL : Store::XML_PATH_UNSECURE_BASE_URL;
-            $backendUrl = $this->config->getValue($xmlPath);
-            if ($backendUrl === null) {
-                $backendUrl = $this->scopeConfig->getValue(
-                    $xmlPath,
-                    ScopeInterface::SCOPE_STORE
-                );
-            }
-        }
+        $backendUrl = $this->getBackendUrl();
+
         $this->uri->parse($backendUrl);
         $configuredHost = $this->uri->getHost();
         if (!$configuredHost) {
@@ -154,4 +144,20 @@ class FrontNameResolver implements FrontNameResolverInterface
 
         return strcasecmp($configuredHost, $host) === 0;
     }
+
+    /**
+     * Retrieve backend URL
+     * @return string|null
+     */
+    private function getBackendUrl(): ?string
+    {
+        if ($this->scopeConfig->getValue(self::XML_PATH_USE_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE)) {
+            return $this->scopeConfig->getValue(self::XML_PATH_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE);
+        }
+
+        $xmlPath = $this->request->isSecure() ? Store::XML_PATH_SECURE_BASE_URL : Store::XML_PATH_UNSECURE_BASE_URL;
+        $backendUrl = $this->config->getValue($xmlPath);
+        return $backendUrl ?? $this->scopeConfig->getValue($xmlPath, ScopeInterface::SCOPE_STORE);
+    }
+
 }
