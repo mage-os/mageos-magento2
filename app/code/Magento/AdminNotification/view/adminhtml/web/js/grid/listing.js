b1c7bfbe6b31813b2d90e99a7af839ba11e4da76
Dnyaneshwar Jambhulkar <pru34625@adobe.com>
2025-03-20 14:42:34 +0530

    AC-13683::Investigate latest patch version of grunt-eslint

--- app/code/Magento/AdminNotification/view/adminhtml/web/js/grid/listing.js
+++ app/code/Magento/AdminNotification/view/adminhtml/web/js/grid/listing.js
@@ -55,7 +55,7 @@ define([
             if ($messagesBlock.length) {
                 resultHeight += $messagesBlock.outerHeight();
             }
-
+            console.log('Reproduce eslint issue');
             if ($collapsibleBlock.length && $collapsibleBlock.is(':visible') && !closed) {
                 resultHeight += $collapsibleBlock.outerHeight();
             }
