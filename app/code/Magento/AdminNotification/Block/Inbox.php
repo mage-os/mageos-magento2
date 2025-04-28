f46b41f6aa09a573ed7e3be9b03ad56b40cc4d13
Alexandra Zota <zota@adobe.com>
2025-04-28 15:26:19 +0300

    ACP2E-3862: update copyrights from AdminAnalytics to CmsUrlRewriteGraphQl including

--- app/code/Magento/AdminNotification/Block/Inbox.php
+++ app/code/Magento/AdminNotification/Block/Inbox.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Adminhtml AdminNotification inbox grid
- *
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2013 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\AdminNotification\Block;
 
--- app/code/Magento/AdminNotification/Controller/Adminhtml/Notification.php
+++ app/code/Magento/AdminNotification/Controller/Adminhtml/Notification.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Adminhtml AdminNotification controller
- *
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2013 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\AdminNotification\Controller\Adminhtml;
 
