2e3f99ca6379c3779444b240e414e61476d24ba3
Oleksandr Rykh <orykh@adobe.com>
2025-05-01 10:52:48 -0700

    ACP2E-3807: [Cloud] Recent Orders not appearing in other store view if the orders are created on one store view

--- app/code/Magento/Sales/Block/Order/Recent.php
+++ app/code/Magento/Sales/Block/Order/Recent.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2011 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Sales\Block\Order;
 
@@ -90,9 +90,6 @@ class Recent extends \Magento\Framework\View\Element\Template
         )->addAttributeToFilter(
             'customer_id',
             $customerId
-        )->addAttributeToFilter(
-            'store_id',
-            $this->storeManager->getStore()->getId()
         )->addAttributeToFilter(
             'status',
             ['in' => $this->_orderConfig->getVisibleOnFrontStatuses()]
