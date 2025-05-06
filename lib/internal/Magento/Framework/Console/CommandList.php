ac753ca8e8d87fefda8e870240c09b76c7d921fc
engcom-Dash <grp-engcom-vendorworker-Dash@adobe.com>
2025-05-06 13:22:45 +0530

    Fixed Static Failure & review comments

--- lib/internal/Magento/Framework/Console/CommandList.php
+++ lib/internal/Magento/Framework/Console/CommandList.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
  */
 
 namespace Magento\Framework\Console;
@@ -39,7 +39,7 @@ class CommandList implements CommandListInterface
     /**
      * @inheritdoc
      */
-    public function getCommands(): array
+    public function getCommands()
     {
         return $this->commands;
     }
