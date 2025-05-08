97f94d2c315b28efaf658d2ee046a765aaf4edcc
Oleksandr Iegorov <oiegorov@adobe.com>
2025-05-08 11:44:40 -0500

    ACP2E-3763: Investigate if AC-3637 can be automated

--- dev/tests/integration/testsuite/Magento/CatalogImportExport/Model/Import/ProductTest/ProductOptionsTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogImportExport/Model/Import/ProductTest/ProductOptionsTest.php
@@ -10,7 +10,6 @@ namespace Magento\CatalogImportExport\Model\Import\ProductTest;
 use Magento\Catalog\Api\Data\ProductCustomOptionInterface;
 use Magento\Catalog\Api\ProductCustomOptionRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\Product;
 use Magento\Catalog\Helper\Data as CatalogConfig;
 use Magento\Catalog\Test\Fixture\Product as ProductFixture;
 use Magento\CatalogImportExport\Model\Import\ProductTestBase;
@@ -246,75 +245,6 @@ class ProductOptionsTest extends ProductTestBase
             $actualIds,
             'Option IDs changed after second import'
         );
-    }
-
-    /**
-     * Test for custom options based on AC-3637
-     *
-     * @dataProvider productsWithCustomOptionsDataProvider
-     * @param string $filename
-     * @param string $sku
-     * @param int $numOfCustomOptions
-     * @throws LocalizedException
-     * @throws NoSuchEntityException
-     *
-     * @return void
-     */
-    public function testImportWithoutCustomOptions(string $filename, string $sku, int $numOfCustomOptions): void
-    {
-        $pathToFile = __DIR__ . '/../_files/' . $filename;
-        $importModel = $this->createImportModel($pathToFile, \Magento\ImportExport\Model\Import::BEHAVIOR_ADD_UPDATE);
-        $errors = $importModel->validateData();
-        $this->assertTrue($errors->getErrorsCount() == 0);
-        $importModel->importData();
-
-        /** @var ProductRepositoryInterface $productRepository */
-        $productRepository = Bootstrap::getObjectManager()->create(ProductRepositoryInterface::class);
-        $product = $productRepository->get($sku);
-
-        $this->assertInstanceOf(Product::class, $product);
-        $options = $product->getOptionInstance()->getProductOptions($product);
-        $this->assertCount($numOfCustomOptions, $options);
-
-        try {
-            $this->productRepository->delete($product);
-        } catch (NoSuchEntityException $e) {
-            //already deleted
-        }
-    }
-
-    /**
-     * @return array
-     */
-    public static function productsWithCustomOptionsDataProvider(): array
-    {
-        return [
-            [
-                'filename' => '001_simple1_no_custom_options.csv',
-                'sku' => 'simple1',
-                'numOfCustomOptions' => 0,
-            ],
-            [
-                'filename' => '002_simple1_4_custom_options.csv',
-                'sku' => 'simple1',
-                'numOfCustomOptions' => 4,
-            ],
-            [
-                'filename' => '003_simple1_5_custom_options.csv',
-                'sku' => 'simple1',
-                'numOfCustomOptions' => 5,
-            ],
-            [
-                'filename' => '004_simple1_5_custom_options_1_updated.csv',
-                'sku' => 'simple1',
-                'numOfCustomOptions' => 5,
-            ],
-            [
-                'filename' => '005_simple1_no_custom_options.csv',
-                'sku' => 'simple1',
-                'numOfCustomOptions' => 0,
-            ],
-        ];
     }
 
     /**
