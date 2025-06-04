1960b3f8f6f07f7b7c6be0f5673c2086a48aef79
Sumesh P <sumesh.p@globallogic.com>
2025-06-04 17:08:37 +0530

    AC-13719: Cache Keys associated with FPC on Magento 2.4.7 multi-store implementations
    Enabled the plugin Magento\PageCache\Model\App\CacheIdentifierPlugin responsible for adding MAGE_RUN_TYPE and MAGE_RUN_CODE prefix to the cache key

---
+++ dev/tests/integration/testsuite/Magento/PageCache/Model/App/CacheIdentifierPluginTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright 2025 Adobe
+ * All rights reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\PageCache\Model\App;
+
+use Magento\Framework\App\PageCache\Identifier;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\PageCache\Model\Cache\Type;
+use Magento\Store\Model\StoreManager;
+use Magento\Store\Test\Fixture\Group as StoreGroupFixture;
+use Magento\Store\Test\Fixture\Store as StoreFixture;
+use Magento\Store\Test\Fixture\Website as WebsiteFixture;
+use Magento\TestFramework\Fixture\Config as ConfigFixture;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorage;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Fixture\DbIsolation;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Integration test for \Magento\PageCache\Model\App\CacheIdentifierPlugin
+ */
+class CacheIdentifierPluginTest extends TestCase
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var Http
+     */
+    private $request;
+
+    /**
+     * @var Identifier
+     */
+    private $identifier;
+
+    /**
+     * @var DataFixtureStorage
+     */
+    private $fixtures;
+
+    protected function setUp(): void
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->request = $this->objectManager->get(Http::class);
+        $this->identifier = $this->objectManager->get(Identifier::class);
+        $this->fixtures = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
+
+        // Enable the cache type
+        $this->objectManager->get(\Magento\Framework\App\Cache\StateInterface::class)
+            ->setEnabled(Type::TYPE_IDENTIFIER, true);
+    }
+
+    /**
+     * Test that cache identifier includes run type and run code
+     */
+    #[
+        DbIsolation(false),
+        ConfigFixture('system/full_page_cache/caching_application', '1', 'store'),
+        ConfigFixture('system/full_page_cache/enabled', '1', 'store'),
+        DataFixture(WebsiteFixture::class, as: 'website'),
+        DataFixture(StoreGroupFixture::class, ['website_id' => '$website.id$'], 'store_group'),
+        DataFixture(StoreFixture::class, ['store_group_id' => '$store_group.id$'], 'store')
+    ]
+    public function testAfterGetValueWithRunTypeAndCode()
+    {
+        $storeCode = $this->fixtures->get('store')->getCode();
+        $serverParams = [
+            StoreManager::PARAM_RUN_TYPE => 'store',
+            StoreManager::PARAM_RUN_CODE => $storeCode
+        ];
+        $this->request->setServer(new \Laminas\Stdlib\Parameters($serverParams));
+
+        $result = $this->identifier->getValue();
+        $this->assertNotEmpty($result);
+        $this->assertStringContainsString('MAGE_RUN_TYPE=store', $result);
+        $this->assertStringContainsString('MAGE_RUN_CODE=' . $storeCode, $result);
+    }
+}
