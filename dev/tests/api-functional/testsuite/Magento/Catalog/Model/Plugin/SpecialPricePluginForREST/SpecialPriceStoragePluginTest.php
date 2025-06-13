616d6ec6d6e13cf8d74126a0671d7ae6ea9ae727
del02433 <del02433@adobe.com>
2025-06-13 13:53:14 +0530

    added test coverage

---
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Model/Plugin/SpecialPricePluginForREST/SpecialPriceStoragePluginTest.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\Catalog\Model\Plugin\SpecialPricePluginForREST;
+
+use Magento\Authorization\Test\Fixture\Role as RoleFixture;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Api\SpecialPriceStorageInterface;
+use Magento\Framework\Webapi\Rest\Request;
+use Magento\Integration\Api\AdminTokenServiceInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorage;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\TestFramework\TestCase\WebapiAbstract;
+use Magento\User\Test\Fixture\User as UserFixture;
+
+/**
+ * WebAPI test to validate SpecialPriceStoragePlugin behavior
+ */
+class SpecialPriceStoragePluginTest extends WebapiAbstract
+{
+    /**
+     * @var DataFixtureStorage
+     */
+    private $fixtures;
+
+    /**
+     * @var SpecialPriceStorageInterface
+     */
+    private SpecialPriceStorageInterface $specialPriceStorage;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private StoreManagerInterface $storeManager;
+
+    protected function setUp(): void
+    {
+        $this->fixtures = Bootstrap::getObjectManager()->get(DataFixtureStorageManager::class)->getStorage();
+        $objectManager = Bootstrap::getObjectManager();
+        $this->specialPriceStorage = $objectManager->get(SpecialPriceStorageInterface::class);
+        $this->productRepository = $objectManager->get(ProductRepositoryInterface::class);
+        $this->storeManager = $objectManager->get(StoreManagerInterface::class);
+
+    }
+
+    #[
+        DataFixture(ProductFixture::class, as: 'product'),
+        DataFixture(RoleFixture::class, as: 'restrictedRole'),
+        DataFixture(UserFixture::class, ['role_id' => '$restrictedRole.id$'], 'restrictedUser'),
+        DataFixture(
+            \Magento\Customer\Test\Fixture\Customer::class,
+            [
+                'email' => 'john@doe.com',
+                'password' => 'test@123',
+                'addresses' => [
+                    [
+                        'country_id' => 'US',
+                        'region_id' => 32,
+                        'city' => 'Boston',
+                        'street' => ['10 Milk Street'],
+                        'postcode' => '02108',
+                        'telephone' => '1234567890',
+                        'default_billing' => true,
+                        'default_shipping' => true,
+                    ],
+                ],
+            ],
+            'customer'
+        ),
+    ]
+    public function testSpecialPriceIsAppliedToAllStoresInWebsite(): void
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $product = $this->fixtures->get('product');
+        $sku = $product->getSku();
+        $storeId= $product->getStoreId();
+        $product->setSpecialPrice(123.45);
+
+        $Store = $this->storeManager->getStore($storeId);
+        $website = $Store->getWebsite();
+        $storeIds = $website->getStoreIds();
+
+        $customer = $this->fixtures->get('customer');
+        $restrictedUser = $this->fixtures->get('restrictedUser');
+
+        $adminTokens = Bootstrap::getObjectManager()->get(AdminTokenServiceInterface::class);
+        $accessToken = $adminTokens->createAdminAccessToken(
+            $restrictedUser->getData('username'),
+            \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD
+        );
+
+
+        $data = [
+            'sku' => $sku,
+            'price' =>123.45,
+            'store_id' => $storeId
+        ];
+
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => '/V1/products/special-price',
+                'httpMethod' => Request::HTTP_METHOD_POST,
+                'token' => $accessToken,
+            ],
+        ];
+        $response = $this->_webApiCall(
+            $serviceInfo,
+            [
+                'prices' => [
+                    $data
+                ]
+            ]
+        );
+        foreach ($storeIds as $storeId) {
+            $product = $this->productRepository->get($sku, false, $storeId);
+            $this->assertNotNull(
+                $product->getSpecialPrice(),
+                "Expected special price for SKU '$sku' in store ID $storeId, but got none."
+            );
+            $this->assertEquals(
+                123.45,
+                (float)$product->getSpecialPrice(),
+                "Special price mismatch for store ID $storeId"
+            );
+        }
+    }
+}
