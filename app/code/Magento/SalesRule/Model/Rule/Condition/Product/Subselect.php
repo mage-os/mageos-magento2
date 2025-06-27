a07aabd86f789169f8a236fd3138a661732acc9b
Oleksandr Rykh <orykh@adobe.com>
2025-06-27 12:36:30 -0700

    ACP2E-3986: After applying ACP2E-3841, free shipping isn't always applied properly for multi address checkout
    - added unit test

--- app/code/Magento/SalesRule/Model/Rule/Condition/Product/Subselect.php
+++ app/code/Magento/SalesRule/Model/Rule/Condition/Product/Subselect.php
@@ -168,7 +168,9 @@ class Subselect extends Combine
                 $subSelectConditionsFlag = $this->validateSubSelectConditions($item);
             }
             $total = $this->getBaseRowTotalForChildrenProduct($item, $attr, $total);
-            if ($subSelectConditionsFlag && $this->validateAttribute($total)) return true;
+            if ($subSelectConditionsFlag && $this->validateAttribute($total)) {
+                return true;
+            }
         }
         return $subSelectConditionsFlag && $this->validateAttribute($total);
     }
