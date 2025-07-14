4374e7f23d679af6c6d2c876774a3b3b02ea9c54
Abhishek Pathak <wip44850@adobe.com>
2025-07-14 17:59:32 +0530

    AC-15054: Product Add to Cart issue in Rest API

--- app/code/Magento/Quote/Plugin/UpdateCartId.php
+++ app/code/Magento/Quote/Plugin/UpdateCartId.php
@@ -1,16 +1,21 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
  */
 
 declare(strict_types=1);
 
 namespace Magento\Quote\Plugin;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Webapi\Rest\Request as RestRequest;
 use Magento\Quote\Api\Data\CartItemInterface;
 use Magento\Quote\Api\GuestCartItemRepositoryInterface;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+use Magento\Store\Model\StoreManagerInterface;
 
 /**
  * Update cart id from request param
@@ -18,16 +23,17 @@ use Magento\Quote\Api\GuestCartItemRepositoryInterface;
 class UpdateCartId
 {
     /**
-     * @var RestRequest $request
-     */
-    private $request;
-
-    /**
      * @param RestRequest $request
+     * @param ProductRepositoryInterface $productRepository
+     * @param StoreManagerInterface $storeManager
+     * @param QuoteIdMaskFactory $quoteIdMaskFactory
      */
-    public function __construct(RestRequest $request)
-    {
-        $this->request = $request;
+    public function __construct(
+        private readonly RestRequest $request,
+        private readonly ProductRepositoryInterface $productRepository,
+        private readonly StoreManagerInterface      $storeManager,
+        private readonly QuoteIdMaskFactory         $quoteIdMaskFactory
+    ) {
     }
 
     /**
@@ -35,17 +41,59 @@ class UpdateCartId
      *
      * @param GuestCartItemRepositoryInterface $guestCartItemRepository
      * @param CartItemInterface $cartItem
-     * @return void
+     * @return array
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function beforeSave(
         GuestCartItemRepositoryInterface $guestCartItemRepository,
         CartItemInterface $cartItem
-    ): void {
+    ): array {
         $cartId = $this->request->getParam('cartId');
 
         if ($cartId) {
             $cartItem->setQuoteId($cartId);
+        }
+        $this->validateProductWebsiteAssignment($cartItem);
+        return [$cartItem];
+    }
+
+    /**
+     * Validate that product is assigned to the current website
+     *
+     * @param CartItemInterface $cartItem
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    private function validateProductWebsiteAssignment(CartItemInterface $cartItem): void
+    {
+        $sku = $cartItem->getSku();
+        if (!$sku) {
+            return;
+        }
+
+        // Get current website ID from the masked cart ID
+        $maskedQuoteId = $cartItem->getQuoteId();
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($maskedQuoteId, 'masked_id');
+
+        if (!$quoteIdMask->getQuoteId()) {
+            return;
+        }
+        $currentWebsiteId = $this->storeManager->getStore()->getWebsiteId();
+        try {
+            $product = $this->productRepository->get($sku, false, null);
+
+            $productWebsiteIds = $product->getWebsiteIds();
+
+            // Validate website assignment
+            if (!is_array($productWebsiteIds) || !in_array($currentWebsiteId, $productWebsiteIds)) {
+                throw new LocalizedException(
+                    __('Product that you are trying to add is not available.')
+                );
+            }
+        } catch (NoSuchEntityException $e) {
+            throw new LocalizedException(
+                __('Product that you are trying to add is not available.')
+            );
         }
     }
 }
