64180fcadde8fa6bcd9b312cf054030045433c26
Sokly Meach <meach@adobe.com>
2025-07-22 14:13:47 -0500

    ACP2E-4049: Unknown IPNs from PayPal abuses application IPN processor

---
+++ app/code/Magento/Paypal/Model/Exception/UnknownIpnException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
+ */
+namespace Magento\Paypal\Model\Exception;
+
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * Exception for unknown or invalid PayPal IPN requests
+ */
+class UnknownIpnException extends LocalizedException
+{
+}
