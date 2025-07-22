81b5ae3b5de7074dfedb2fe6d925139ee04c6c25
Bhavin Parmar <cod62627@adobe.com>
2025-07-22 19:19:16 +0530

    AC-15170:: Add official support for Symfony 7.4 LTS in Adobe Commerce

--- app/code/Magento/Downloadable/Console/Command/DomainsRemoveCommand.php
+++ app/code/Magento/Downloadable/Console/Command/DomainsRemoveCommand.php
@@ -67,7 +67,7 @@ class DomainsRemoveCommand extends Command
     /**
      * @inheritdoc
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         try {
             if ($input->getArgument(self::INPUT_KEY_DOMAINS)) {
