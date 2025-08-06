a9c7c5bd6ebb6c6f52010ab698ce5bdfb6e3f59a
Sokly Meach <meach@adobe.com>
2025-08-06 13:31:40 -0500

    ACP2E-4100: Regular price is not visible, even though a special price is applied.

--- app/code/Magento/Catalog/Test/Unit/Pricing/Price/SpecialPriceBulkResolverTest.php
+++ app/code/Magento/Catalog/Test/Unit/Pricing/Price/SpecialPriceBulkResolverTest.php
@@ -21,6 +21,9 @@ use Magento\Store\Model\StoreManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class SpecialPriceBulkResolverTest extends TestCase
 {
     /**
