7aa6fcbca0e54e6c3cfc4811b0e733abae1ca8b2
Alexandra Zota <zota@adobe.com>
2025-09-03 15:43:33 +0300

    ACP2E-4132: [Cloud] Deactivate the old sitemap generation

--- app/code/Magento/Sitemap/Plugin/Cron/Model/Config/Backend/SitemapPlugin.php
+++ app/code/Magento/Sitemap/Plugin/Cron/Model/Config/Backend/SitemapPlugin.php
@@ -9,6 +9,8 @@ namespace Magento\Sitemap\Plugin\Cron\Model\Config\Backend;
 
 use Magento\Cron\Model\Config\Backend\Sitemap;
 use Magento\Cron\Model\Config\Source\Frequency;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\ValueFactory;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Sitemap\Model\Config\Source\GenerationMethod;
 
@@ -28,15 +30,15 @@ class SitemapPlugin
     private const CRON_MODEL_PATH = 'crontab/default/jobs/sitemap_generate/run/model';
 
     /**
-     * @var \Magento\Framework\App\Config\ValueFactory
+     * @var ValueFactory
      */
     private $configValueFactory;
 
     /**
-     * @param \Magento\Framework\App\Config\ValueFactory $configValueFactory
+     * @param ValueFactory $configValueFactory
      */
     public function __construct(
-        \Magento\Framework\App\Config\ValueFactory $configValueFactory
+        ValueFactory $configValueFactory
     ) {
         $this->configValueFactory = $configValueFactory;
     }
@@ -55,6 +57,28 @@ class SitemapPlugin
     ) {
         $config = $subject->getConfig();
 
+        $time = $this->getTimeConfiguration($subject, $config);
+        $frequency = $subject->getValue();
+        $generationMethod = $this->getGenerationMethod($subject, $config);
+
+        $cronExprString = $this->buildCronExpression($time, $frequency);
+        $observerModel = $this->getObserverModel($generationMethod);
+
+        $this->updateCronConfiguration($cronExprString, $observerModel);
+
+        return $result;
+    }
+
+    /**
+     * Get time configuration from subject or config
+     *
+     * @param Sitemap $subject
+     * @param ScopeConfigInterface|null $config
+     * @return array
+     * @throws LocalizedException
+     */
+    private function getTimeConfiguration(Sitemap $subject, $config): array
+    {
         $time = $subject->getData('groups/generate/fields/time/value');
         if (!$time && $config) {
             $timeConfig = $config->getValue(
@@ -62,11 +86,51 @@ class SitemapPlugin
                 $subject->getScope(),
                 $subject->getScopeId()
             );
-            $time = $timeConfig ? explode(',', $timeConfig) : ['0', '0', '0'];
+            $time = $timeConfig ? explode(',', $timeConfig) : null;
         }
 
-        $frequency = $subject->getValue();
+        if (!$time) {
+            $recentTimeConfig = $this->getRecentlySavedTimeConfiguration();
+            $time = $recentTimeConfig ? explode(',', $recentTimeConfig) : null;
+        }
 
+        if (!is_array($time) || empty($time)) {
+            $time = ['0', '0', '0'];
+        }
+
+        while (count($time) < 3) {
+            $time[] = '0';
+        }
+
+        return $time;
+    }
+
+    /**
+     * Get recently saved time configuration from config value factory
+     *
+     * @return string|null
+     * @throws LocalizedException
+     */
+    private function getRecentlySavedTimeConfiguration(): ?string
+    {
+        $configValue = $this->configValueFactory->create()->load(
+            'sitemap/generate/time',
+            'path'
+        );
+
+        return $configValue->getId() ? $configValue->getValue() : null;
+    }
+
+    /**
+     * Get generation method from various sources
+     *
+     * @param Sitemap $subject
+     * @param ScopeConfigInterface|null $config
+     * @return string
+     * @throws LocalizedException
+     */
+    private function getGenerationMethod(Sitemap $subject, ?ScopeConfigInterface $config): string
+    {
         $generationMethod = $subject->getData('groups/generate/fields/generation_method/value');
 
         if (!$generationMethod && $config) {
@@ -75,18 +139,40 @@ class SitemapPlugin
                 $subject->getScope(),
                 $subject->getScopeId()
             );
-        } elseif (!$generationMethod && !$config) {
-            $configValue = $this->configValueFactory->create()->load(
-                'sitemap/generate/generation_method',
-                'path'
-            );
-            if ($configValue->getId()) {
-                $generationMethod = $configValue->getValue();
-            }
         }
 
-        $generationMethod = $generationMethod ?: GenerationMethod::STANDARD;
+        if (!$generationMethod) {
+            $generationMethod = $this->getRecentlySavedGenerationMethod();
+        }
 
+        return $generationMethod ?: GenerationMethod::STANDARD;
+    }
+
+    /**
+     * Get recently saved generation method from config value factory
+     *
+     * @return string|null
+     * @throws LocalizedException
+     */
+    private function getRecentlySavedGenerationMethod(): ?string
+    {
+        $configValue = $this->configValueFactory->create()->load(
+            'sitemap/generate/generation_method',
+            'path'
+        );
+
+        return $configValue->getId() ? $configValue->getValue() : null;
+    }
+
+    /**
+     * Build cron expression from time and frequency
+     *
+     * @param array $time
+     * @param string $frequency
+     * @return string
+     */
+    private function buildCronExpression(array $time, string $frequency): string
+    {
         $cronExprArray = [
             (int)($time[1] ?? 0), //Minute
             (int)($time[0] ?? 0), //Hour
@@ -95,23 +181,41 @@ class SitemapPlugin
             $frequency == Frequency::CRON_WEEKLY ? '1' : '*', //# Day of the Week
         ];
 
-        $cronExprString = join(' ', $cronExprArray);
+        return join(' ', $cronExprArray);
+    }
 
+    /**
+     * Get observer model based on generation method
+     *
+     * @param string $generationMethod
+     * @return string
+     */
+    private function getObserverModel(string $generationMethod): string
+    {
+        return $generationMethod === GenerationMethod::BATCH
+            ? 'Magento\Sitemap\Model\Batch\Observer::scheduledGenerateSitemaps'
+            : 'Magento\Sitemap\Model\Observer::scheduledGenerateSitemaps';
+    }
+
+    /**
+     * Update cron configuration with new values
+     *
+     * @param string $cronExprString
+     * @param string $observerModel
+     * @return void
+     * @throws LocalizedException
+     */
+    private function updateCronConfiguration(string $cronExprString, string $observerModel): void
+    {
         try {
             $this->clearCronConfiguration(self::CRON_STRING_PATH);
             $this->clearCronConfiguration(self::CRON_MODEL_PATH);
-
-            $observerModel = $generationMethod === GenerationMethod::BATCH
-                ? 'Magento\Sitemap\Model\Batch\Observer::scheduledGenerateSitemaps'
-                : 'Magento\Sitemap\Model\Observer::scheduledGenerateSitemaps';
 
             $this->setCronConfiguration(self::CRON_STRING_PATH, $cronExprString);
             $this->setCronConfiguration(self::CRON_MODEL_PATH, $observerModel);
         } catch (\Exception $e) {
             throw new LocalizedException(__('We can\'t save the cron expression.'));
         }
-
-        return $result;
     }
 
     /**
@@ -120,6 +224,7 @@ class SitemapPlugin
      * @param string $path
      * @param string $value
      * @return void
+     * @throws LocalizedException
      */
     private function setCronConfiguration(string $path, string $value): void
     {
@@ -138,6 +243,7 @@ class SitemapPlugin
      *
      * @param string $path
      * @return void
+     * @throws LocalizedException
      */
     private function clearCronConfiguration(string $path): void
     {
