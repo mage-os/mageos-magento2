f905ec14601bc1b8b4bc28f7b2da2f71ea795f02
Mohit Sharma <glo87054@adobe.com>
2025-09-10 15:54:25 +0530

    ACQE-8696: [Web API Automation] - AC-8972
    - Write new web api automation test for product not returned when visibility is catalog only

---
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductVisibilitySearchTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\GraphQl\Catalog;
+
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\TestCase\GraphQlAbstract;
+
+class ProductVisibilitySearchTest extends GraphQlAbstract
+{
+    #[DataFixture(
+        ProductFixture::class,
+        ['price' => 1, 'name' => 'API_TEST', 'visibility' => Visibility::VISIBILITY_IN_CATALOG],
+        'prod1'
+    )]
+    public function testProductNotReturnedWhenVisibilityIsCatalogOnly(): void
+    {
+        $query = <<<'QUERY'
+query {
+  products(
+    search: "API"
+    filter: { price: { from: "1" } }
+  ) {
+    items {
+      name
+      sku
+    }
+  }
+}
+QUERY;
+
+        $response = $this->graphQlQuery($query);
+        $this->assertArrayNotHasKey('errors', $response);
+        $this->assertEmpty(
+            $response['products']['items'],
+            'Expected no products to be returned when product is set to Catalog-only visibility.'
+        );
+    }
+}
