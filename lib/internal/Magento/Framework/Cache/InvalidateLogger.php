ba810477fddde093ee0c19ab54ab5031fb78d4a1
Luke Rodgers <lukerodgers90@gmail.com>
2025-09-18 16:57:04 +0100

    Update copyright header
    https://public-results-storage-prod.magento-testing-service.engineering/reports/magento/magento2/pull/40196/0b62a7b8683ef732e82264c00c926c60/Statics/report-sanity-ee.html
    ```
    /**
     * Copyright {{CREATION_YEAR}} Adobe
     * All Rights Reserved.
     */
    
    ```

--- lib/internal/Magento/Framework/Cache/InvalidateLogger.php
+++ lib/internal/Magento/Framework/Cache/InvalidateLogger.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Cache configuration model. Provides cache configuration data to the application
- *
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Framework\Cache;
 
