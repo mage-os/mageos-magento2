26bc7c2db99fd4a060b8baff628cc96468a481c5
DeepaAdobe <del71107@adobe.com>
2025-09-19 17:46:06 +0530

    AC-15634 [CE] PHPUnit 12: Upgrade Catalog & Product Management related test cases: Phpmd and stan issue fixes

--- lib/internal/Magento/Framework/TestFramework/Unit/Helper/ObjectManager.php
+++ lib/internal/Magento/Framework/TestFramework/Unit/Helper/ObjectManager.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+* Copyright 2012 Adobe
+* All Rights Reserved.
+*/
 namespace Magento\Framework\TestFramework\Unit\Helper;
 
 use Magento\Framework\GetParameterClassTrait;
