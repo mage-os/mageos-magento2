374d31af8b88025f5840ead509e79f0455658dbb
Anna Bukatar <abukatar@adobe.com>
2025-09-29 22:05:04 -0700

    ACP2E-4231: Fotorama can open Mini Cart on Image Viewer close action

--- dev/tests/js/jasmine/tests/lib/web/fotorama/stopEvent.test.js
+++ dev/tests/js/jasmine/tests/lib/web/fotorama/stopEvent.test.js
@@ -15,18 +15,25 @@ define([
         var originalMatchMedia, originalModernizr, stopEventFn;
 
         function extractStopEvent(source) {
-            var start = source.indexOf('function stopEvent');
+            let start = source.indexOf('function stopEvent'),
+                braceStart,
+                depth,
+                i,
+                end,
+                ch;
+
             if (start === -1) {
                 return undefined;
             }
-            var braceStart = source.indexOf('{', start);
-            var depth = 0, i = braceStart, end = -1;
+            braceStart = source.indexOf('{', start);
+            depth = 0, i = braceStart, end = -1;
             for (; i < source.length; i++) {
-                var ch = source[i];
+                ch = source[i];
                 if (ch === '{') {
                     depth++;
                 } else if (ch === '}') {
                     depth--;
+                    // eslint-disable-next-line max-depth
                     if (depth === 0) {
                         end = i + 1;
                         break;
@@ -37,7 +44,7 @@ define([
                 return undefined;
             }
             // eslint-disable-next-line no-new-func
-            return (new Function('return (' + source.slice(start, end) + ');'))();
+            return new Function('return (' + source.slice(start, end) + ');')();
         }
 
         beforeAll(function () {
@@ -66,33 +73,37 @@ define([
         }
 
         it('prevents default and stops propagation on mobile touchend', function () {
-            window.matchMedia = function () { return { matches: true }; };
             var e = mockEvent('touchend');
+
+            window.matchMedia = function () { return { matches: true }; };
             stopEventFn(e);
             expect(e.preventDefault).toHaveBeenCalled();
             expect(e.stopPropagation).toHaveBeenCalled();
         });
 
         it('prevents default on desktop click when Modernizr.touch is false', function () {
+            var e = mockEvent('click');
+
             window.matchMedia = function () { return { matches: false }; };
             window.Modernizr.touch = false;
-            var e = mockEvent('click');
             stopEventFn(e);
             expect(e.preventDefault).toHaveBeenCalled();
         });
 
         it('does not prevent default on desktop click when Modernizr.touch is true', function () {
+            var e = mockEvent('click');
+
             window.matchMedia = function () { return { matches: false }; };
             window.Modernizr.touch = true;
-            var e = mockEvent('click');
             stopEventFn(e);
             expect(e.preventDefault).not.toHaveBeenCalled();
         });
 
         it('stops propagation when second argument is true', function () {
+            var e = mockEvent('click');
+
             window.matchMedia = function () { return { matches: false }; };
             window.Modernizr.touch = false;
-            var e = mockEvent('click');
             stopEventFn(e, true);
             expect(e.stopPropagation).toHaveBeenCalled();
         });
