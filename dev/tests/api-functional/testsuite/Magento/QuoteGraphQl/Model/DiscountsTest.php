61e8edda43f63f41b60ba28013654bd607f175b6
Sokly Meach <meach@adobe.com>
2025-10-10 16:57:12 -0500

    ACP2E-4248: [GraphQl] cart query cart item discount issue on virtual quotes

--- dev/tests/api-functional/testsuite/Magento/QuoteGraphQl/Model/DiscountsTest.php
+++ dev/tests/api-functional/testsuite/Magento/QuoteGraphQl/Model/DiscountsTest.php
@@ -15,6 +15,9 @@ use Magento\Integration\Api\CustomerTokenServiceInterface;
 use Magento\Quote\Test\Fixture\AddProductToCart as AddProductToCartFixture;
 use Magento\Quote\Test\Fixture\CustomerCart;
 use Magento\Quote\Test\Fixture\QuoteIdMask;
+use Magento\SalesRule\Model\Rule;
+use Magento\SalesRule\Model\Rule\Condition\Product;
+use Magento\SalesRule\Model\Rule\Condition\Product\Combine;
 use Magento\SalesRule\Test\Fixture\Rule as SalesRuleFixture;
 use Magento\TestFramework\Fixture\DataFixture;
 use Magento\TestFramework\Fixture\DataFixtureStorageManager;
@@ -74,20 +77,65 @@ class DiscountsTest extends GraphQlAbstract
     }
 
     #[
-        DataFixture(VirtualProductFixture::class, as: 'product'),
+        DataFixture(
+            VirtualProductFixture::class,
+            ['sku' => 'virtual111', 'price' => 100, 'category_ids' => [2]],
+            as: 'product1'
+        ),
+        DataFixture(
+            VirtualProductFixture::class,
+            ['sku' => 'virtual222', 'price' => 100, 'category_ids' => [2]],
+            as: 'product2'
+        ),
         DataFixture(Customer::class, ['email' => 'customer@example.com'], as: 'customer'),
         DataFixture(CustomerCart::class, ['customer_id' => '$customer.id$'], as: 'cart'),
-        DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product.id$']),
-        DataFixture(QuoteIdMask::class, ['cart_id' => '$cart.id$'], as: 'quoteIdMask'),
-        DataFixture(SalesRuleFixture::class, ['discount_amount' => 10, 'simple_action' => 'by_percent'], as: 'rule')
+        DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product1.id$']),
+        DataFixture(AddProductToCartFixture::class, ['cart_id' => '$cart.id$', 'product_id' => '$product2.id$']),
+        DataFixture(QuoteIdMask::class, ['cart_id' => '$cart.id$'], as: 'quoteIdMask')
     ]
     /**
-     * Test discounts resolver for a virtual quote
+     * Test discounts resolver for a virtual quote with conditional discount on specific SKU
      */
     public function testDiscountsVirtualQuote()
     {
-        $maskedQuoteId = DataFixtureStorageManager::getStorage()->get('quoteIdMask')->getMaskedId();
-        $query = $this->getCartDiscountsQuery($maskedQuoteId);
+        // Using SalesRule DataFixture is not working for this case
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var Rule $rule */
+        $rule = $objectManager->create(Rule::class);
+        $rule->setName('10% off for virtual222')
+            ->setIsActive(1)
+            ->setSimpleAction('by_percent');
+        $rule->loadPost([
+            'name' => '10% off for virtual222',
+            'is_active' => 1,
+            'simple_action' => 'by_percent',
+            'discount_amount' => 10,
+            'website_ids' => [1],
+            'customer_group_ids' => [0, 1, 2, 3],
+            'actions' => [
+                1 => [
+                    'type' => Combine::class,
+                    'attribute' => null,
+                    'operator' => null,
+                    'value' => '1',
+                    'is_value_processed' => null,
+                    'aggregator' => 'all',
+                    'actions' => [
+                        1 => [
+                            'type' => Product::class,
+                            'attribute' => 'sku',
+                            'operator' => '==',
+                            'value' => 'virtual222',
+                            'is_value_processed' => false,
+                        ]
+                    ]
+                ]
+            ],
+        ]);
+        $rule->save();
+        $quoteIdMask = DataFixtureStorageManager::getStorage()->get('quoteIdMask');
+        $maskedQuoteId = $quoteIdMask->getMaskedId();
+        $query = $this->getCartDiscountsQueryWithItems($maskedQuoteId);
         $response = $this->graphQlQuery(
             $query,
             [],
@@ -95,19 +143,34 @@ class DiscountsTest extends GraphQlAbstract
             $this->getHeaderMap()
         );
         $cartData = $response['cart'];
-        $discounts = $cartData['prices']['discounts'] ?? [];
-        $this->assertNotEmpty($discounts);
-        $this->assertCount(1, $discounts);
-        $discount = $discounts[0];
+        $this->assertCount(2, $cartData['items']);
+        $virtual1Item = null;
+        $virtual2Item = null;
+        foreach ($cartData['items'] as $item) {
+            if ($item['product']['sku'] === 'virtual111') {
+                $virtual1Item = $item;
+            } elseif ($item['product']['sku'] === 'virtual222') {
+                $virtual2Item = $item;
+            }
+        }
+
+        $this->assertNotNull($virtual1Item, 'virtual1 item not found in cart');
+        $this->assertNotNull($virtual2Item, 'virtual2 item not found in cart');
+        $this->assertEmpty($virtual1Item['prices']['discounts'], 'virtual111 should not have any discounts');
+        $this->assertNotEmpty($virtual2Item['prices']['discounts'], 'virtual222 should have discounts');
+        $this->assertCount(1, $virtual2Item['prices']['discounts']);
+        $discount = $virtual2Item['prices']['discounts'][0];
         $this->assertArrayHasKey('label', $discount);
         $this->assertArrayHasKey('amount', $discount);
         $this->assertArrayHasKey('value', $discount['amount']);
-        $this->assertArrayHasKey('currency', $discount['amount']);
-        $this->assertArrayHasKey('applied_to', $discount);
         $this->assertEquals('Discount', $discount['label']);
-        $this->assertGreaterThan(0, $discount['amount']['value']);
-        $this->assertEquals('USD', $discount['amount']['currency']);
-        $this->assertEquals('ITEM', $discount['applied_to']);
+        $this->assertEquals(10, $discount['amount']['value']);
+        $cartDiscounts = $cartData['prices']['discounts'] ?? [];
+        $this->assertNotEmpty($cartDiscounts, 'Cart should have discounts');
+        $this->assertCount(1, $cartDiscounts);
+        $this->assertEquals('Discount', $cartDiscounts[0]['label']);
+        $this->assertEquals('ITEM', $cartDiscounts[0]['applied_to']);
+        $this->assertEquals(10, $cartDiscounts[0]['amount']['value']);
     }
 
     #[
@@ -146,6 +209,53 @@ class DiscountsTest extends GraphQlAbstract
         return <<<QUERY
         query {
             cart(cart_id: "{$maskedQuoteId}") {
+                prices {
+                    discounts {
+                        label
+                        amount {
+                            value
+                            currency
+                        }
+                        applied_to
+                    }
+                }
+            }
+        }
+        QUERY;
+    }
+
+    /**
+     * Get cart discounts query with items
+     *
+     * @param string $maskedQuoteId
+     * @return string
+     */
+    private function getCartDiscountsQueryWithItems(string $maskedQuoteId): string
+    {
+        return <<<QUERY
+        query {
+            cart(cart_id: "{$maskedQuoteId}") {
+                items {
+                    product {
+                        sku
+                        name
+                    }
+                    prices {
+                        price {
+                            value
+                        }
+                        row_total {
+                            value
+                        }
+                        discounts {
+                            label
+                            amount {
+                                value
+                                currency
+                            }
+                        }
+                    }
+                }
                 prices {
                     discounts {
                         label
