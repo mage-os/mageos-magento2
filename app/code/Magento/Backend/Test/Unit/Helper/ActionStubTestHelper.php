d3fa2c5306a2a985a0bb8d89dd83aacf7c88e944
Manish Ranjan <del55204@adobe.com>
2025-10-13 17:31:33 +0530

    AC-15182: PHPUnit 12 Upgrade Removed not needed helper files

--- app/code/Magento/Backend/Test/Unit/Helper/ActionStubTestHelper.php
+++ app/code/Magento/Backend/Test/Unit/Helper/ActionStubTestHelper.php
@@ -10,6 +10,13 @@ namespace Magento\Backend\Test\Unit\Helper;
 use Magento\Backend\Test\Unit\App\Action\Stub\ActionStub;
 
 /**
+ * Test helper for ActionStub with additional test-specific methods
+ *
+ * This helper extends ActionStub to provide the setDirtyRulesNoticeMessage()
+ * method which is used in production by CatalogRule controllers but doesn't
+ * exist in the base ActionStub class. Since PHPUnit 12 removed addMethods(),
+ * this helper is necessary to test code that calls this method.
+ *
  * @SuppressWarnings(PHPMD.AllPurposeAction)
  */
 class ActionStubTestHelper extends ActionStub
@@ -20,6 +27,13 @@ class ActionStubTestHelper extends ActionStub
     }
     
     /**
+     * Stub method for testing CatalogRule controllers
+     *
+     * This method is called by AdminGws\Model\Controllers::promoCatalogIndexAction()
+     * but doesn't exist in the base ActionStub class.
+     *
+     * @param string $message
+     * @return $this
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function setDirtyRulesNoticeMessage($message)
