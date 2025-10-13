124a1f96865eb3c8fcd810929fb60300a2e31400
Anna Bukatar <abukatar@adobe.com>
2025-10-13 13:50:54 -0700

    ACP2E-4245: Bundle product price issue on cart page - Item price was converted twice

--- app/code/Magento/Tax/Block/Item/Price/Renderer.php
+++ app/code/Magento/Tax/Block/Item/Price/Renderer.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2014 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Tax\Block\Item\Price;
 
@@ -209,11 +209,7 @@ class Renderer extends \Magento\Framework\View\Element\Template
     public function getItemDisplayPriceExclTax()
     {
         $item = $this->getItem();
-        if ($item instanceof QuoteItem) {
-            return $item->getCalculationPrice();
-        } else {
-            return $item->getPrice();
-        }
+        return $item->getPrice();
     }
 
     /**
