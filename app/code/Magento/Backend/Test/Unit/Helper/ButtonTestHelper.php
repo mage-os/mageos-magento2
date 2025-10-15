c8cf83057e32b195020bbc96e2bc16545ff1ea75
Manish Ranjan <del55204@adobe.com>
2025-10-15 22:12:48 +0530

    AC-15182: PHPUnit 12 Upgrade | Removed Helper Files

--- app/code/Magento/Backend/Test/Unit/Helper/ButtonTestHelper.php
+++ app/code/Magento/Backend/Test/Unit/Helper/ButtonTestHelper.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -54,4 +54,3 @@ class ButtonTestHelper extends Button
         return $this->htmlCount === 1 ? 'block_html_' : 'another_block_html';
     }
 }
-
