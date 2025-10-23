5636c0864725453eecbde89d457a37deb283ece5
Raj Mohan R <cod43156@adobe.com>
2025-10-23 14:49:39 +0530

    AC-15800::[CE] PHPUnit 12: Refactor Inline Anonymous Classes to Helper Classes in PR #2559 & PR#2579 | Add PHPUnit 12 test helpers for Catalog Rule

---
+++ app/code/Magento/Catalog/Test/Unit/Helper/ProductActionTestHelper.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\Catalog\Test\Unit\Helper;
+
+use Magento\Catalog\Model\Product\Action;
+
+/**
+ * TestHelper for Product Action with dynamic methods
+ */
+class ProductActionTestHelper extends Action
+{
+    /** @var array */
+    private $attributesData = [];
+    /** @var array */
+    private $productIds = [];
+
+    public function __construct()
+    {
+        // Skip parent constructor to avoid complex dependencies
+    }
+
+    public function getAttributesData()
+    {
+        return $this->attributesData;
+    }
+
+    public function setAttributesData($value)
+    {
+        $this->attributesData = $value;
+        return $this;
+    }
+
+    public function getProductIds()
+    {
+        return $this->productIds;
+    }
+
+    public function setProductIds($value)
+    {
+        $this->productIds = $value;
+        return $this;
+    }
+}
---
+++ lib/internal/Magento/Framework/Stdlib/Test/Unit/Helper/DateTestHelperForCatalogRule.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Magento\Framework\Stdlib\Test\Unit\Helper;
+
+use Magento\Framework\Stdlib\DateTime\DateTime;
+
+/**
+ * TestHelper for Date with dynamic methods
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
+class DateTestHelperForCatalogRule extends DateTime
+{
+    /** @var string|null */
+    private $formatValue = null;
+
+    public function __construct()
+    {
+        // Skip parent constructor to avoid complex dependencies
+    }
+
+    public function format($format)
+    {
+        return $this->formatValue;
+    }
+
+    public function setFormatValue($value)
+    {
+        $this->formatValue = $value;
+        return $this;
+    }
+}
