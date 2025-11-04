f763fe506e3ea558f6777aad4151e1633d7186f7
Sokly Meach <meach@adobe.com>
2025-11-04 10:39:02 -0600

    ACP2E-4311: Error loading some locale

--- app/code/Magento/Customer/Test/Unit/Block/Widget/DobTest.php
+++ app/code/Magento/Customer/Test/Unit/Block/Widget/DobTest.php
@@ -447,8 +447,7 @@ class DobTest extends TestCase
             $validationRule->expects($this->any())
                 ->method('getValue')
                 ->willReturn(strtotime(self::MIN_DATE));
-        }
-        elseif ($type=="MAX") {
+        } elseif ($type=="MAX") {
             $validationRule->expects($this->any())
                 ->method('getName')
                 ->willReturn(Dob::MAX_DATE_RANGE_KEY);
@@ -456,8 +455,6 @@ class DobTest extends TestCase
                 ->method('getValue')
                 ->willReturn(strtotime(self::MAX_DATE));
         }
-
-
         return $validationRule;
     }
 
