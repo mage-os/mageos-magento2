ab2a6a09cb0bf2878eda9c1699f6023faec4f05b
del02433 <del02433@adobe.com>
2025-11-27 14:31:05 +0530

    AC-15670 : When scheduled update for category is deleted, amount of children is not decreased for the parent category

--- app/code/Magento/Catalog/Model/ResourceModel/Category/AggregateCount.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Category/AggregateCount.php
@@ -28,10 +28,37 @@ class AggregateCount
          * Update children count for all parent categories
          */
         $parentIds = $category->getParentIds();
+        $childrenCount = $this->getCategoryRowCount($resourceModel, $category->getId());
         if ($parentIds) {
-            $data = ['children_count' => new \Zend_Db_Expr('children_count - 1')];
+            $data = ['children_count' => new \Zend_Db_Expr('children_count - '.$childrenCount)];
             $where = ['entity_id IN(?)' => $parentIds];
             $resourceModel->getConnection()->update($resourceModel->getEntityTable(), $data, $where);
         }
     }
+
+    /**
+     * To get count of rows (category count) for a specific entity ID
+     *
+     * @param \Magento\Catalog\Model\ResourceModel\Category $resourceModel
+     * @param int $categoryId
+     * @return int
+     */
+    public function getCategoryRowCount(
+        \Magento\Catalog\Model\ResourceModel\Category $resourceModel,
+        $categoryId
+    ): int {
+        $connection = $resourceModel->getConnection();
+        $table      = $resourceModel->getEntityTable();
+
+        // Tell staging NOT to add created_in / updated_in filters
+        $select = $connection->select();
+        $select->setPart('disable_staging_preview', true);
+
+        $select->from($table, ['cnt' => new \Zend_Db_Expr('COUNT(*)')])
+            ->where('entity_id = ?', (int)$categoryId);
+
+        return (int)$connection->fetchOne($select);
+    }
+
+
 }
