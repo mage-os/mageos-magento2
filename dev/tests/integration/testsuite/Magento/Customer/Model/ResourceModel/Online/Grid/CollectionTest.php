6d9cbfd148735fd8baed440ccac9b655d49a77d5
Banvari Lal <143788813+glo60612@users.noreply.github.com>
2025-12-11 13:29:05 +0530

    AC-11511: Customers Now Online Admin Grid Shows Duplicate Rows Every Time a User Logs In, Then Out, Then In

--- dev/tests/integration/testsuite/Magento/Customer/Model/ResourceModel/Online/Grid/CollectionTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Model/ResourceModel/Online/Grid/CollectionTest.php
@@ -3,6 +3,8 @@
  * Copyright 2025 Adobe
  * All Rights Reserved.
  */
+declare(strict_types=1);
+
 namespace Magento\Customer\Model\ResourceModel\Online\Grid;
 
 use Magento\Customer\Test\Fixture\Customer;
