813496ee749e411dfa88acf3f06eea706923b254
Francis <furan917@users.noreply.github.com>
2025-12-14 14:45:17 +0100

    Feature: Add command to detect unregistered themes (#186)
    * feat: Add theme:registration:status command to detect unregistered themes
    
    * chore: small linting updates

---
+++ app/code/Magento/Theme/Console/Command/ThemeRegistrationStatusCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Copyright © Mage-OS, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\Theme\Console\Command;
+
+use Magento\Framework\Console\Cli;
+use Magento\Theme\Model\Theme\RegistrationDetector;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command for uninstalling theme and backup-code feature
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+ */
+class ThemeRegistrationStatusCommand extends Command
+{
+
+    public const EXIT_CODE_THEME_UPDATE_REQUIRED = 2;
+
+    /**
+     * Constructor
+     *
+     * @param RegistrationDetector $registrationDetector
+     */
+    public function __construct(
+        private RegistrationDetector $registrationDetector
+    ) {
+        $this->registrationDetector = $registrationDetector;
+        parent::__construct();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('theme:registration:status');
+        $this->setDescription('Displays the registration status of themes.');
+        parent::configure();
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if ($this->registrationDetector->hasUnregisteredTheme()) {
+            $missing = $this->registrationDetector->getMissingThemes();
+            $output->writeln('<info>Unregistered themes detected:</info>');
+            foreach ($missing as $theme) {
+                $output->writeln("  - $theme");
+            }
+            $output->writeln('<comment>Run setup:upgrade to register themes.</comment>');
+            return self::EXIT_CODE_THEME_UPDATE_REQUIRED;
+        }
+
+        $output->writeln('<info>All themes are registered.</info>');
+        return Cli::RETURN_SUCCESS;
+    }
+}
