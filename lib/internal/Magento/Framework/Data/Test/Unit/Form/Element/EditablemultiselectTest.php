48c347e81c084b360fcff80f7a56e996012b65a0
Evgeniy Rybkin <helloevgeniyrybkin@gmail.com>
2026-03-09 18:53:28 +0300

    40564: Added test cases to check disabled option state in the 'EditablemultiselectTest' test

--- lib/internal/Magento/Framework/Data/Test/Unit/Form/Element/EditablemultiselectTest.php
+++ lib/internal/Magento/Framework/Data/Test/Unit/Form/Element/EditablemultiselectTest.php
@@ -80,4 +80,65 @@ class EditablemultiselectTest extends TestCase
         $this->assertStringContainsString('ElementControl = new CustomSelect(', $elementHtml);
         $this->assertStringContainsString('ElementControl.init();', $elementHtml);
     }
+
+    #[DataProvider('disabledOptionDataProvider')]
+    public function testOptionDisabledStateIsRenderedCorrectly(array $option, bool $shouldBeDisabled): void
+    {
+        $this->_model->setValues([$option]);
+
+        $optionHtml = $this->extractOptionHtmlByValue($this->_model->getElementHtml(), $option['value']);
+
+        $this->assertNotEmpty($optionHtml);
+
+        if ($shouldBeDisabled) {
+            $this->assertStringContainsString('disabled="disabled"', $optionHtml);
+        } else {
+            $this->assertStringNotContainsString('disabled="disabled"', $optionHtml);
+        }
+    }
+
+    #[DataProvider('disabledOptionDataProvider')]
+    public function testOptgroupOptionDisabledStateIsRenderedCorrectly(array $option, bool $shouldBeDisabled): void
+    {
+        $this->_model->setValues([
+            [
+                'label' => 'Group',
+                'value' => [$option],
+            ],
+        ]);
+
+        $optionHtml = $this->extractOptionHtmlByValue($this->_model->getElementHtml(), $option['value']);
+
+        $this->assertNotEmpty($optionHtml);
+
+        if ($shouldBeDisabled) {
+            $this->assertStringContainsString('disabled="disabled"', $optionHtml);
+        } else {
+            $this->assertStringNotContainsString('disabled="disabled"', $optionHtml);
+        }
+    }
+
+    public static function disabledOptionDataProvider(): iterable
+    {
+        yield 'explicitly_disabled' => [['value' => '1', 'label' => 'Option', 'disabled' => true], true];
+        yield 'disabled_flag_as_int_1' => [['value' => '1', 'label' => 'Option', 'disabled' => 1], true];
+        yield 'disabled_flag_as_string_foo' => [['value' => '1', 'label' => 'Option', 'disabled' => 'foo'], true];
+        yield 'disabled_flag_as_string_1' => [['value' => '1', 'label' => 'Option', 'disabled' => '1'], true];
+
+        yield 'explicitly_enabled' => [['value' => '1', 'label' => 'Option', 'disabled' => false], false];
+        yield 'disabled_flag_as_int_0' => [['value' => '1', 'label' => 'Option', 'disabled' => 0], false];
+        yield 'disabled_flag_as_string_0' => [['value' => '1', 'label' => 'Option', 'disabled' => '0'], false];
+        yield 'disabled_flag_as_empty_string' => [['value' => '1', 'label' => 'Option', 'disabled' => ''], false];
+        yield 'disabled_flag_as_null' => [['value' => '1', 'label' => 'Option', 'disabled' => null], false];
+        yield 'disabled_flag_not_provided' => [['value' => '1', 'label' => 'Option'], false];
+    }
+
+    private function extractOptionHtmlByValue(string $elementHtml, string $value): string
+    {
+        $pattern = '/<option[^>]*value="' . preg_quote($value, '/') . '"[^>]*>.*?<\/option>/s';
+
+        preg_match($pattern, $elementHtml, $matches);
+
+        return $matches[0] ?? '';
+    }
 }
