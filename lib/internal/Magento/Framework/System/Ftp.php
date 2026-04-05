79bfa45ab7f724d0cc314dbd669a5a024072e548
Ryan Hoerr <rhoerr@users.noreply.github.com>
2026-04-05 17:58:00 -0400

    Removed trailing space

--- lib/internal/Magento/Framework/System/Ftp.php
+++ lib/internal/Magento/Framework/System/Ftp.php
@@ -8,7 +8,7 @@ namespace Magento\Framework\System;
 
 /**
  * Class to work with remote FTP server
- * 
+ *
  * @deprecated
  * @see This class depended on the PHP ext-ftp extension, which has been removed.
  */
