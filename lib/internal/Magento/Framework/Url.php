b0382f723bf8b7bccf4c7c8776794c9c2ca83c34
Allan Fernandes <allanmfx@gmail.com>
2026-06-01 16:28:54 -0300

    Include scope ID in Url cache key to prevent multi-store URL collision
    Url::_getUrl() cached URLs by sha1(routePath + params) with no store
    scope. In multi-store setups the same route and params produce different
    URLs per store (different base URLs, rewrites, suffixes). When the Url
    instance scope changed via setScope(), the stale cache entry for the
    previous store was returned instead of generating the correct URL for
    the new store.
    
    Appending the current scope ID to the cache key isolates each store's
    URL cache entries from one another.

--- lib/internal/Magento/Framework/Url.php
+++ lib/internal/Magento/Framework/Url.php
@@ -873,7 +873,7 @@ class Url extends \Magento\Framework\DataObject implements \Magento\Framework\Ur
             ksort($cachedParams);
         }
 
-        $cacheKey = sha1($routePath . $this->serializer->serialize($cachedParams));
+        $cacheKey = sha1($routePath . $this->serializer->serialize($cachedParams) . ($this->_getScope() ? $this->_getScope()->getId() : 0));
         if (!isset($this->cacheUrl[$cacheKey])) {
             $this->cacheUrl[$cacheKey] = $this->getUrlModifier()->execute(
                 $this->createUrl($routePath, $routeParams)
