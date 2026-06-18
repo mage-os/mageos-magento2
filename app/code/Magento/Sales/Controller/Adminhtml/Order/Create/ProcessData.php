47e8e476d1fa98a802c1f5f5106d9922504f9464
Sokly Meach <meach@adobe.com>
2026-06-18 12:39:08 -0500

    ACP2E-5046: Product quantities modified on page refresh while creating order from admin

--- app/code/Magento/Sales/Controller/Adminhtml/Order/Create/ProcessData.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Order/Create/ProcessData.php
@@ -10,12 +10,12 @@ class ProcessData extends \Magento\Sales\Controller\Adminhtml\Order\Create
     /**
      * Process data and display index page
      *
-     * @return \Magento\Backend\Model\View\Result\Forward
+     * @return \Magento\Framework\Controller\Result\Redirect
      */
     public function execute()
     {
         $this->_initSession();
         $this->_processData();
-        return $this->resultForwardFactory->create()->forward('index');
+        return $this->resultRedirectFactory->create()->setPath('sales/*');
     }
 }
